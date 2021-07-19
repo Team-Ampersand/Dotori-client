c74b6e1546905955417c37f062064c170b868ace
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-07-19 14:40:15 +0900

    [Add] TodaySong

---
+++ src/Components/TodaySong/Style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Postioner = styled.div`
+margin-right: 20px;
+flex: 0.55;
+display: flex;
+background-color: #ffffff;
+box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 20px;
+    label {
+        margin: 4% 4.5%;
+        font-size: 26px;
+        line-height: 29px;
+        font-weight: bold;
+        margin-right: auto;
+    }
+`;
