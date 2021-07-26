d14ed1f07543c143fce58311587e18601449649c
송시현 <75899332+Songsihyeon@users.noreply.github.com>
2021-07-26 17:28:49 +0900

    [Add] PostObj type

--- src/Pages/NoticePage/NoticePage.tsx
+++ src/Pages/NoticePage/NoticePage.tsx
@@ -4,13 +4,21 @@ import { MatchType } from '../../Utils/GlobalType';
 import PageTemplate from 'Pages/Template/PageTemplate';
 
 interface NoticeProps {
-  match: MatchType
+  match: MatchType;
+}
+
+type PostObj = {
+  board_id: number,
+  user_id: number,
+  board_title: string,
+  board_content: string,
+  board_time: number
 }
 
 const NoticePage: React.FC<NoticeProps> = ({ match }) => {
   return (
     <PageTemplate match={match}>
-      
+
     </PageTemplate>
   )
 }
