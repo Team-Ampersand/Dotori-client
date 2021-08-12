c61318202af16e4296c07cc6afd693d619197ee7
송시현 <75899332+Songsihyeon@users.noreply.github.com>
2021-08-12 23:28:40 +0900

    [Fix] prettier

--- src/Components/NoticeBoard/NoticeBoard.tsx
+++ src/Components/NoticeBoard/NoticeBoard.tsx
@@ -7,12 +7,12 @@ const NoticeBoard: React.FC = () => {
     <div>
       <S.Positioner>
         <S.Container>
-          <NoticeBoardItem board_key={1} author="teacher" date="20210619" />
-          <NoticeBoardItem board_key={2} author="teacher" date="20210619" />
-          <NoticeBoardItem board_key={3} author="student" date="20210619" />
-          <NoticeBoardItem board_key={4} author="teacher" date="20210619" />
-          <NoticeBoardItem board_key={5} author="student" date="20210619" />
-          <NoticeBoardItem board_key={6} author="student" date="20210619" />
+          <NoticeBoardItem board_key={1} author="teacher" />
+          <NoticeBoardItem board_key={2} author="teacher" />
+          <NoticeBoardItem board_key={3} author="student" />
+          <NoticeBoardItem board_key={4} author="teacher" />
+          <NoticeBoardItem board_key={5} author="student" />
+          <NoticeBoardItem board_key={6} author="student" />
         </S.Container>
       </S.Positioner>
     </div>
