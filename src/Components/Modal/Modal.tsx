781fb3f991865a04aa62dac7aceab2a1a05c94e8
송시현 <75899332+Songsihyeon@users.noreply.github.com>
2021-08-12 23:33:06 +0900

    [Add] notice item modal
    공지사항 항목에 대한 모달을 퍼블리싱했습니다.

---
+++ src/Components/Modal/Modal.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as S from './Style';
+
+interface ModalProps {
+	board_key: number;
+	modalState: boolean;
+	closeMoal: (e: any) => void;
+	authorColor: string;
+}
+
+const Modal: React.FC<ModalProps> = ({
+	board_key,
+	modalState,
+	closeMoal,
+	authorColor,
+}) => {
+	return modalState ? (
+		<div>
+			<S.Positioner>
+				<S.Overlay onClick={(e) => closeMoal(e)} />
+				<S.Container>
+					<S.Title authorColor={authorColor}>
+						이건 테스트 제목입니다.
+						<S.Close onClick={(e) => closeMoal(e)}>X</S.Close>
+					</S.Title>
+					<S.Content>
+						가나다라마 아에이오우 자음과 모음이 만나서 글자를 만들어요
+						가나다라마 아에이오우 그 글자들로 우린 얘기 해~
+					</S.Content>
+				</S.Container>
+			</S.Positioner>
+		</div>
+	) : (
+		<></>
+	);
+};
+
+export default Modal;
