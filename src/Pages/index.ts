42b84619c5527741e883f01da57295e5be7f974a
송시현 <75899332+Songsihyeon@users.noreply.github.com>
2021-09-01 08:37:35 +0900

    [Add] export NoticeWritePage

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -4,18 +4,20 @@ import BannerPage from './BannerPage/BannerPage';
 import PageTemplate from './Template/PageTemplate';
 import LaptopPage from './LaptopPage/LaptopPage';
 import NoticePage from './NoticePage/NoticePage';
+import NoticeWritePage from './NoticeWritePage/NoticeWritePage';
 import AdminLaptopListPage from './AdminLaptopListPage/AdminLaptopListPage';
 import SongPage from './SongPage/SongPage';
 import AdminSongListPage from './AdminSongListPage/AdminSongListPage';
 
-export { 
-    SidebarPage,
-    HomePage,
-    BannerPage,
-    PageTemplate,
-    LaptopPage,
-    NoticePage,
-    AdminLaptopListPage,
-    SongPage,
-    AdminSongListPage
+export {
+	SidebarPage,
+	HomePage,
+	BannerPage,
+	PageTemplate,
+	LaptopPage,
+	NoticePage,
+	NoticeWritePage,
+	AdminLaptopListPage,
+	SongPage,
+	AdminSongListPage,
 };
