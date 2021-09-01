8202e4b968bb49678b44d2ee73f5fe39c3c4eb03
송시현 <75899332+Songsihyeon@users.noreply.github.com>
2021-09-01 11:28:47 +0900

    prettier

--- src/Components/LaptopTable/Header/Header.tsx
+++ src/Components/LaptopTable/Header/Header.tsx
@@ -3,35 +3,36 @@ import * as S from './Style';
 import { MatchType } from '../../../Utils/GlobalType';
 import { LaptopHeader, SongHeader } from './model/CombineAdminHeader';
 
-
 interface HeaderProps {
-    match: MatchType
+	match: MatchType;
 }
 
-const onlyCompareThisHeader = (match : MatchType) => {
-    switch (match.path) {
-        case "/laptop":
-            return LaptopHeader
-        case "/song":
-            return SongHeader
-        default:
-            break;
-    }
-}
+const onlyCompareThisHeader = (match: MatchType) => {
+	switch (match.path) {
+		case '/laptop':
+			return LaptopHeader;
+		case '/song':
+			return SongHeader;
+		default:
+			break;
+	}
+};
 
-const mappingAdminHeader = (match: MatchType) => 
-    onlyCompareThisHeader(match)!.list.map(item => {
-        return <div style={{ flex: item.flex }}>{item.label}</div>
-    })
+const mappingAdminHeader = (match: MatchType) =>
+	onlyCompareThisHeader(match)!.list.map((item) => {
+		return <div style={{ flex: item.flex }}>{item.label}</div>;
+	});
 
-
-const Header : React.FC<HeaderProps> = ({ match }) => {
-    return (
-        <>
-            {match.path == "/laptop" ? <S.Postioner>{mappingAdminHeader(match)}</S.Postioner> : <S.SongPostioner>{mappingAdminHeader(match)}
-            </S.SongPostioner>}
-        </>
-    )
-}
+const Header: React.FC<HeaderProps> = ({ match }) => {
+	return (
+		<>
+			{match.path === '/laptop' ? (
+				<S.Postioner>{mappingAdminHeader(match)}</S.Postioner>
+			) : (
+				<S.SongPostioner>{mappingAdminHeader(match)}</S.SongPostioner>
+			)}
+		</>
+	);
+};
 
 export default Header;
