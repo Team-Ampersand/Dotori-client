88d636be3a9475c11b9cc1ac56c85f3a824a22f1
송시현 <75899332+Songsihyeon@users.noreply.github.com>
2021-09-01 11:22:59 +0900

    [Add] Notice write page router 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { HomePage, LaptopPage, SongPage, NoticePage } from './Pages';
+import {
+	HomePage,
+	LaptopPage,
+	SongPage,
+	NoticePage,
+	NoticeWritePage,
+} from './Pages';
 
 const Router: React.FC = () => {
-  return (
-    <>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/laptop" component={LaptopPage} />
-      <Route path="/notice" component={NoticePage} />
-      <Route path="/notice/write" component={() => <div>공지사항 작성 페이지</div>} />
-      <Route path="/song" component={SongPage} />
-    </>
-  );
+	return (
+		<>
+			<Route exact path="/" component={HomePage} />
+			<Route path="/laptop" component={LaptopPage} />
+			<Route exact path="/notice" component={NoticePage} />
+			<Route path="/notice/write" component={NoticeWritePage} />
+			<Route path="/song" component={SongPage} />
+		</>
+	);
 };
 
-export default Router
+export default Router;
