7764b398bb37e1da5a78f1f45a55603639373341
송시현 <75899332+Songsihyeon@users.noreply.github.com>
2021-09-01 11:21:50 +0900

    [Add] Notice Write PageTemplate 추가

--- src/Pages/Template/PageTemplate.tsx
+++ src/Pages/Template/PageTemplate.tsx
@@ -1,97 +1,115 @@
-import React, { useState } from "react";
-import * as S from "./Style";
-import BannerPage from "../BannerPage/BannerPage";
-import { MatchType } from "../../Utils/GlobalType";
-import { ManufactureDate } from "../../Utils/ManufactureDate";
-import { ChangeLaptopType, ChangeSongType } from "../../Components";
-import { AdminLaptopListPage, AdminSongListPage } from "../";
+import React, { useState } from 'react';
+import * as S from './Style';
+import BannerPage from '../BannerPage/BannerPage';
+import { MatchType } from '../../Utils/GlobalType';
+import { ManufactureDate } from '../../Utils/ManufactureDate';
+import { ChangeLaptopType, ChangeSongType } from '../../Components';
+import { AdminLaptopListPage, AdminSongListPage } from '../';
 
 interface TemplateProps {
-  match: MatchType;
+	match: MatchType;
 }
 
 const returnPageType = (routerName: string) => {
-  switch (routerName) {
-    case "/":
-      return "홈";
-    case "/laptop":
-      return "노트북 대여";
-    case '/song':
-      return '기상음악';
-    case '/notice':
-      return '공지사항';
-    default:
-      break;
-  }
+	switch (routerName) {
+		case '/':
+			return '홈';
+		case '/laptop':
+			return '노트북 대여';
+		case '/song':
+			return '기상음악';
+		case '/notice':
+			return '공지사항';
+		case '/notice/write':
+			return '공지사항 작성';
+		default:
+			break;
+	}
 };
 
 const returnValueType = (nowUrl: string) => {
-  switch (nowUrl) {
-    case "/laptop":
-      return "laptop";
-    case '/song':
-      return "song";
-    case '/notice':
-      return "notice"; 
-    default:
-      return 0;
-  }
-}
+	switch (nowUrl) {
+		case '/laptop':
+			return 'laptop';
+		case '/song':
+			return 'song';
+		case '/notice':
+			return 'notice';
+		case '/notice/write':
+			return 'notice write';
+		default:
+			return 0;
+	}
+};
 
-const ChangeType = (match: MatchType, isActive: boolean, setIsActive:React.Dispatch<React.SetStateAction<boolean>>) => {
-  if (match.path === "/laptop") {
-    return <ChangeLaptopType
-      active={isActive}
-      setActive={(value: boolean) => setIsActive(value)}
-    />;
-  } else if (match.path === "/song") {
-    return <ChangeSongType
-      active={isActive}
-      setActive={(value: boolean) => setIsActive(value)}
-    />;
-  }
-}
+const ChangeType = (
+	match: MatchType,
+	isActive: boolean,
+	setIsActive: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+	if (match.path === '/laptop') {
+		return (
+			<ChangeLaptopType
+				active={isActive}
+				setActive={(value: boolean) => setIsActive(value)}
+			/>
+		);
+	} else if (match.path === '/song') {
+		return (
+			<ChangeSongType
+				active={isActive}
+				setActive={(value: boolean) => setIsActive(value)}
+			/>
+		);
+	}
+};
 
-const BannerStatus = (match: MatchType, children: React.ReactNode, isActive: boolean) => {
-  if (returnValueType(match.path) === "notice") {
-    return (
-      <>
-        <BannerPage match={match} />
-        <S.Content>{children}</S.Content>
-      </>)
-    ;
-  } else if (!isActive) {
-    return (<>
-      <S.Banner>
-        <BannerPage match={match} />
-      </S.Banner>
-      <S.Content>{children}</S.Content>
-    </>);
-  } else if (returnValueType(match.path) === "laptop") {
-    return (<AdminLaptopListPage match={match} />);
-  } else if (returnValueType(match.path) === "song") {
-    return (<AdminSongListPage match={match} />);
-  }
-}
+const BannerStatus = (
+	match: MatchType,
+	children: React.ReactNode,
+	isActive: boolean
+) => {
+	if (returnValueType(match.path) === 'notice' || 'notice write') {
+		return (
+			<>
+				<BannerPage match={match} />
+				<S.Content>{children}</S.Content>
+			</>
+		);
+	} else if (!isActive) {
+		return (
+			<>
+				<S.Banner>
+					<BannerPage match={match} />
+				</S.Banner>
+				<S.Content>{children}</S.Content>
+			</>
+		);
+	} else if (returnValueType(match.path) === 'laptop') {
+		return <AdminLaptopListPage match={match} />;
+	} else if (returnValueType(match.path) === 'song') {
+		return <AdminSongListPage match={match} />;
+	}
+};
 
 const PageTemplate: React.FC<TemplateProps> = ({ match, children }) => {
-  const [isActive, setIsActive] = useState(false);
+	const [isActive, setIsActive] = useState(false);
 
-  return (
-    <S.Postioner>
-      <S.Wrapper>
-        <S.Title>
-          {returnPageType(match.path)}
-          <strong>
-            {ManufactureDate("Y")}년 {ManufactureDate("M")}월{" "}
-            {ManufactureDate("D")}일 {ManufactureDate("W")}요일
-          </strong>
-          {ChangeType(match, isActive, setIsActive)}
-        </S.Title>
-        {BannerStatus(match, children, isActive)}
-      </S.Wrapper>
-    </S.Postioner>
-  );
+	return (
+		<S.Postioner>
+			<S.Wrapper>
+				<S.Title>
+					{returnPageType(match.path)}
+					<strong>
+						{ManufactureDate('Y')}년 {ManufactureDate('M')}월{' '}
+						{ManufactureDate('D')}일 {ManufactureDate('W')}요일
+					</strong>
+					{ChangeType(match, isActive, setIsActive)}
+				</S.Title>
+				{BannerStatus(match, children, isActive)}
+			</S.Wrapper>
+		</S.Postioner>
+	);
 };
 
 export default PageTemplate;
