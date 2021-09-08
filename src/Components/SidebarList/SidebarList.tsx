b8315eea8d16c252cdf71c4c79c95bc48c8a7fa0
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-09-08 20:48:37 +0900

    [Update] sidebar 문제 해결

--- src/Components/SidebarList/SidebarList.tsx
+++ src/Components/SidebarList/SidebarList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SidebarItem from '../SidebarItem/SidebarItem';
 import * as S from './Style';
 
@@ -16,12 +16,28 @@ interface SidebarListProps {
 	setCurrnentRouter: (currentRouter: string) => void;
 }
 
+
+
+const returnSidebarColor = (router : string) => {
+	if(window.location.pathname === router) return true;
+	else return false;
+}
+
+
 const SidebarList: React.FC<SidebarListProps> = ({
 	menuName,
 	menuDataLists,
 	currentRouter,
 	setCurrnentRouter,
 }) => {
+	
+	useEffect(() => {
+		console.log(window.location.pathname);
+		window.onpopstate = () => {
+			setCurrnentRouter(window.location.pathname);
+		}
+	},[window.location.pathname])
+
 	const MappingSidebarItem = menuDataLists.map((item) => (
 		<SidebarItem
 			router={item.router}
