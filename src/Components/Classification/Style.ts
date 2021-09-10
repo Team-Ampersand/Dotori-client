d092012a67aa5677cfd9a25692fae629085fb39a
송시현 <75899332+Songsihyeon@users.noreply.github.com>
2021-09-10 19:20:40 +0900

    주석 삭제

--- src/Components/Classification/Style.ts
+++ src/Components/Classification/Style.ts
@@ -1,31 +1,5 @@
 import styled from "styled-components";
 
-// export const BtnWrapper = styled.div`
-// 	display: flex;
-// 	flex-direction: column;
-// 	margin-top: 10px;
-// `;
-
-// export const RowWrapper = styled.div`
-// 	display: row;
-// `;
-
-// export const Btn = styled.button`
-// 	width: 120px;
-// 	height: 50px;
-// 	margin: 7px;
-// 	outline: none;
-// 	border: none;
-// 	border-radius: 10px;
-// 	background-color: gray;
-// 	color: #fff;
-// 	font-size: 20px;
-// 	font-weight: bold;
-// 	&:hover {
-// 		background-color: #0f4c81;
-// 	}
-// `;
-
 export const SelectWrapper = styled.div`
 	display: flex;
 	flex-direction: row;
