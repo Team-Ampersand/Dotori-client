f520f889fb3f715c63b4dd6557eddaf927f7af89
송시현 <75899332+Songsihyeon@users.noreply.github.com>
2021-09-10 16:09:29 +0900

    [Add] Classification select box

--- src/Components/Classification/Classification.tsx
+++ src/Components/Classification/Classification.tsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./Style";
 
 const Classification: React.FC = () => {
+	const onSubmit = () => {
+		console.log(stuGrade + stuClass);
+	};
+
+	const [stuGrade, setStuGrade] = useState("");
+	const gradeSelect = (e) => {
+		setStuGrade(e.target.value);
+	};
+
+	const [stuClass, setStuClass] = useState("");
+	const classSelect = (e) => {
+		setStuClass(e.target.value);
+	};
+
 	return (
 		<>
-			<S.Form>
-				<S.SelectBox>
-					<S.Option selected>선택</S.Option>
-					<S.Option>1</S.Option>
-					<S.Option>2</S.Option>
-					<S.Option>3</S.Option>
+			<S.SelectWrapper>
+				<S.SelectBox onChange={gradeSelect} value={stuGrade}>
+					<S.Option value="0">선택</S.Option>
+					<S.Option value="1">1</S.Option>
+					<S.Option value="2">2</S.Option>
+					<S.Option value="3">3</S.Option>
 				</S.SelectBox>
-				학년
-				<S.SelectBox>
-					<S.Option>선택</S.Option>
-					<S.Option>1</S.Option>
-					<S.Option>2</S.Option>
-					<S.Option>3</S.Option>
-					<S.Option>4</S.Option>
+				<S.Label>학년</S.Label>
+				<S.SelectBox onChange={classSelect} value={stuClass}>
+					<S.Option value="0">선택</S.Option>
+					<S.Option value="1">1</S.Option>
+					<S.Option value="2">2</S.Option>
+					<S.Option value="3">3</S.Option>
+					<S.Option value="4">4</S.Option>
 				</S.SelectBox>
-				반
-			</S.Form>
+				<S.Label>반</S.Label>
+				<S.Btn onClick={onSubmit}>검색</S.Btn>
+			</S.SelectWrapper>
 		</>
 	);
 };
