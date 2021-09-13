d4ebf8393ad6ffc68b0ee9698069d267fb485a19
송시현 <75899332+Songsihyeon@users.noreply.github.com>
2021-09-13 19:51:51 +0900

    [Update] point scoreBoard

--- src/Components/Point/Style.ts
+++ src/Components/Point/Style.ts
@@ -6,3 +6,12 @@ export const Positioner = styled.div`
 	display: flex;
 	flex-direction: column;
 `;
+
+export const scoreBoard = styled.div`
+	margin-top: 50px;
+	border-radius: 20px;
+	width: 1720px;
+	height: 770px;
+	background-color: #fff;
+	overflow: scroll;
+`;
