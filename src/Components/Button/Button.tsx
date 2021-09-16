0630f4603673a270e96dfa61924b07ea21e559c0
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-09-16 17:55:56 +0900

    [Update] Button 기능 추가

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,24 +1,44 @@
-import { LoginArrow } from '../../Assets/Svg'
-import React from 'react'
-import * as S from "./Style"
+import { LoginArrow } from '../../Assets/Svg';
+import React from 'react';
+import * as S from './Style';
+import { useHistory, useLocation } from 'react-router';
 
 const returnButtonValue = (currentPath) => {
-    if(currentPath === "/signin") return "로그인"
-    else if(currentPath === "/signup") return "회원가입"
-    else if(currentPath === "/password") return "임시 비밀번호 발급" 
-}
+	if (currentPath === '/signin') return '로그인';
+	else if (currentPath === '/signup') return '회원가입';
+	else if (currentPath === '/password') return '임시 비밀번호 발급';
+};
+
+const returnParamValue = () => {
+	if (window.location.pathname === '/signin') {
+		return '/';
+	} else if (window.location.pathname === '/signup') {
+		return '/signin';
+	} else if (window.location.pathname === 'password') {
+		return '/signin';
+	} else return '';
+};
 
 const Button: React.FC = () => {
-    return (
-        <>
-            <S.ButtonStyle>
-                {returnButtonValue(window.location.pathname)}
-                <S.ArrowWrapper>
-                    <LoginArrow />
-                </S.ArrowWrapper>
-            </S.ButtonStyle>
-        </>
-    )
-}
+	const history = useHistory();
+	return (
+		<>
+			<S.ButtonStyle
+				onClick={() => {
+					alert(
+						`${returnButtonValue(window.location.pathname)}에 성공하셨습니다.`
+					);
+					history.push(returnParamValue());
+					window.location.reload();
+				}}
+			>
+				{returnButtonValue(window.location.pathname)}
+				<S.ArrowWrapper>
+					<LoginArrow />
+				</S.ArrowWrapper>
+			</S.ButtonStyle>
+		</>
+	);
+};
 
-export default Button
+export default Button;
