5f5c60cf3febabbe57f1ada0b59b17ca3a77c1d6
송시현 <75899332+Songsihyeon@users.noreply.github.com>
2021-09-16 17:17:08 +0900

    [Update] song page restyle

--- src/Components/SongRequest/Style.ts
+++ src/Components/SongRequest/Style.ts
@@ -1,97 +1,64 @@
 import styled from 'styled-components';
 
 export const Postioner = styled.div`
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #ffffff;
-    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 20px;
-        label {
-            margin: 3.5% 6.5%;
-            margin-right: auto;
-            font-weight: bold;
-            font-size: 26px;
-        }
+	width: 50%;
+	height: 80%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background-color: #ffffff;
+	border-radius: 20px;
+	label {
+		margin: 3.5% 6.5%;
+		margin-right: auto;
+		font-weight: bold;
+		font-size: 26px;
+	}
 `;
 
 export const InputWrapper = styled.div`
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    margin-top: 15vh;
-    @media (max-height: 950px) {
-        max-height: 500px;
-    }
-    @media (max-height: 850px) {
-        max-height: 400px;
-    }
-    @media (max-height: 760px) {
-        max-height: 350px;
-    }
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	margin-top: 15vh;
 
-    p {
-        margin-left: 35px;
-        margin-top: 4%;
-        margin-bottom: 1rem;
-        font-weight: bold;
-        font-size: 18px;
-        line-height: 24px;
+	p {
+		margin-left: 35px;
+		margin-top: 4%;
+		margin-bottom: 1rem;
+		font-weight: bold;
+		font-size: 18px;
+		line-height: 24px;
+	}
+	input {
+		margin-left: 4%;
+		padding-left: 10px;
+		width: 90%;
+		height: 65px;
+		font-size: 20px;
+		border: 2px solid #0f4c81;
+		box-sizing: border-box;
+		border-radius: 15px;
+	}
+	button {
+		margin-top: 50px;
+		margin-left: 291px;
+		margin-right: 310px;
+		width: 160px;
+		height: 50px;
+		border: 0;
+		outline: 0;
+		align-items: center;
+		background: #0f4c81;
+		border-radius: 10px;
+		font-size: 18px;
+		font-weight: bold;
+		color: white;
+	}
 
-        @media (max-height: 950px) {
-            margin-top: 3%;
-            margin-bottom: 13px;
-        }
-        @media (max-height: 850px) {
-            margin-top: 2%;
-            margin-bottom: 10px;
-        }
-        @media (max-height: 760px) {
-            margin-top: 1%;
-        }
-    }
-    input {
-        margin-left: 4%;
-        padding-left: 10px;
-        width: 90%;
-        height: 65px;
-        font-size: 20px;
-        border: 3px solid #B0CBFF;
-        box-sizing: border-box;
-        border-radius: 8px;
-    }
-    button {
-        margin-top: 50px;
-        margin-left: 291px;
-        margin-right: 310px;
-        width: 160px;
-        height: 50px;
-        border: 0;
-        outline: 0;
-        align-items: center;
-        background: #617BE3;
-        border-radius: 10px;
-        font-size: 16px;
-        color: white;
-
-        @media (max-height: 950px) {
-            margin-left: 271px;
-            margin-right: 290px;
-            margin-top: 40px;
-        }
-        @media (max-height: 850px) {
-            margin-left: 251px;
-            margin-right: 270px;
-            margin-top: 30px;
-        }
-        @media (max-height: 760px) {
-            margin-left: 231px;
-            margin-right: 250px;
-            margin-top: 25px;
-        }
-    }
-    button:hover {
-        background-color: #4D6ADB;
-    }
+	button:hover {
+		color: #0f4c81;
+		border: 2px solid #0f4c81;
+		background-color: #fff;
+	}
 `;
--- src/Components/TodaySong/Style.ts
+++ src/Components/TodaySong/Style.ts
@@ -1,39 +1,37 @@
 import styled from 'styled-components';
 
 export const Postioner = styled.div`
-    width: 50%;
-    height: 70vh;
-    flex: 1;
-    margin-right: 20px;
-    display: flex;
-    flex-direction: column;
-    background-color: #ffffff;
-    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 20px;
-    overflow-y: auto;
-    overflow-x: hidden;
-    position: relative;
+	width: 50%;
+	height: 80%;
+	flex: 1;
+	margin-right: 20px;
+	display: flex;
+	flex-direction: column;
+	background-color: #ffffff;
+	border-radius: 20px;
+	overflow-y: auto;
+	overflow-x: hidden;
+	position: relative;
 
-    &::-webkit-scrollbar {
-        display: none;
-    }
+	&::-webkit-scrollbar {
+		display: none;
+	}
 `;
 
 export const PlaylistContainer = styled.div`
-    width: 93.3%;
-    height: 20vh;
-    padding: 3.5% 0 3.5% 6.5%;
-    margin: 0 auto;
-    display: flex;
-    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
-    font-size: 26px;
-    font-weight: bold;
-    position: sticky;
-    top: 0;
-    border-radius: 20px;
-    background-color: #fff;
+	width: 93.3%;
+	height: 20vh;
+	padding: 3.5% 0 3.5% 6.5%;
+	margin: 0 auto;
+	display: flex;
+	font-size: 26px;
+	font-weight: bold;
+	position: sticky;
+	top: 0;
+	border-radius: 20px;
+	background-color: #fff;
 `;
 
 export const SongContainer = styled.div`
-    margin-top: 2vh;
+	margin-top: 2vh;
 `;
