36aa5995fd56ee3ee5869a4ac26e73fc0e7a9ce2
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-11-24 18:14:46 +0900

    [Update] 자습 신청 여부, list recoil 생성

--- src/Atoms/AtomContainer.ts
+++ src/Atoms/AtomContainer.ts
@@ -1,6 +1,26 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
+
+type User = {
+	id: string;
+	stdNum: string;
+	username: string;
+};
 
 export const HasToken = atom<boolean>({
 	key: 'HasToken',
 	default: false,
+});
+
+export const isClicked = atom<boolean>({
+	key: 'isClicked',
+	default: true,
+	effects_UNSTABLE: [persistAtom],
+});
+
+export const list = atom<User[]>({
+	key: 'list',
+	default: [{ id: '', stdNum: '', username: '' }],
 });
