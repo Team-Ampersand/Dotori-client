a99fd6d1d0f0f4ed0214ad8fb8f076bb21d1e62d
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-11-24 18:14:23 +0900

    [Update] 반별 자습 조회 Api 연동

--- src/Components/Classification/Classification.tsx
+++ src/Components/Classification/Classification.tsx
@@ -1,17 +1,40 @@
-import React, { useState } from "react";
-import * as S from "./Style";
+import React, { useState } from 'react';
+import * as S from './Style';
+import selfstudy from 'Api/selfstudy';
+import { useSetRecoilState } from 'recoil';
+import { list } from 'Atoms';
+import role from 'Api/role';
+import { rolelookup } from 'Utils/Libs/roleLookup';
 
 const Classification: React.FC = () => {
-	const onSubmit = () => {
-		console.log(stuGrade + stuClass);
+	const [stuGrade, setStuGrade] = useState('');
+	const [stuClass, setStuClass] = useState('');
+	const setClassLookup = useSetRecoilState(list);
+
+	const onSubmit = async () => {
+		try {
+			if (window.location.pathname === '/selfstudy') {
+				if (stuGrade + stuClass === '00') {
+					const res = await selfstudy.lookupstudy();
+					setClassLookup(res.data.data);
+				} else {
+					const res = await selfstudy.classlookup(stuGrade + stuClass);
+					setClassLookup(res.data.data);
+				}
+			}
+		} catch (e: any) {
+			alert(
+				e.message === 'Error: Request failed with status code 404'
+					? stuGrade + '학년' + stuClass + '반에는 신청한 학생이 없습니다'
+					: e
+			);
+		}
 	};
 
-	const [stuGrade, setStuGrade] = useState("");
 	const gradeSelect = (e) => {
 		setStuGrade(e.target.value);
 	};
 
-	const [stuClass, setStuClass] = useState("");
 	const classSelect = (e) => {
 		setStuClass(e.target.value);
 	};
