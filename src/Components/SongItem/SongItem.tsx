87df9ecf00526f1ee8e44c2915fde9677ed0800b
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-11-30 16:32:41 +0900

    Update Prettier

--- src/Components/SongItem/SongItem.tsx
+++ src/Components/SongItem/SongItem.tsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import * as S from './Style'
+import React from 'react';
+import * as S from './Style';
 
 type SongItemObj = {
-    thumbnail: string,
-    title: string,
-    author: string
-}
+	thumbnail: string;
+	title: string;
+	author: string;
+};
 
 interface SongProps {
-    songObj: SongItemObj[];
+	songObj: SongItemObj[];
 }
 
 const SongItem: React.FC<SongProps> = ({ songObj }) => {
-    return (
-        <>
-            {songObj.map((item, ix) => (
-            <S.Positioner key={`${ix}`}>
-                <S.ImgContainer thumbnail={item.thumbnail}/>
-                <S.Container>
-                    <S.TitleContainer>{item.title}</S.TitleContainer>
-                    <S.AuthorContainer>{item.author}</S.AuthorContainer>    
-                </S.Container>
-            </S.Positioner>
-        ))}
-        </>
-    )
-}
+	return (
+		<>
+			{songObj.map((item, ix) => (
+				<S.Positioner key={`${ix}`}>
+					<S.ImgContainer thumbnail={item.thumbnail} />
+					<S.Container>
+						<S.TitleContainer>{item.title}</S.TitleContainer>
+						<S.AuthorContainer>{item.author}</S.AuthorContainer>
+					</S.Container>
+				</S.Positioner>
+			))}
+		</>
+	);
+};
 
-export default SongItem
+export default SongItem;
