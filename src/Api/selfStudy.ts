28400d2044f1ad56b1182db173a41d71cbc64c46
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-11-30 16:38:24 +0900

    Update selfStudy api

--- src/Api/selfStudy.ts
+++ src/Api/selfStudy.ts
@@ -1,17 +1,50 @@
-import { selfStudyController } from 'Utils/Libs/requestUrls';
-import RequestApi from 'Utils/Libs/requestApi';
+import { rolelookup } from 'Utils/Libs/roleLookup';
+import RequestApi from '../Utils/Libs/requestApi';
+import { MemberSelfstudyController } from '../Utils/Libs/requestUrls';
 
-class selfStudy {
-	async apply() {
-		try {
-			return await RequestApi({
-				method: 'PUT',
-				url: selfStudyController.apply(),
-			});
-		} catch (e: any) {
-			throw new Error(e);
-		}
+class SelfStudy {
+	async selfstudy() {
+		const role = await rolelookup();
+		return RequestApi({
+			method: 'PUT',
+			url: MemberSelfstudyController.selfStudy(role),
+		});
+	}
+	async lookupstudy() {
+		const role = await rolelookup();
+		return RequestApi({
+			method: 'GET',
+			url: MemberSelfstudyController.selfStudy(role),
+		});
+	}
+	async cancelstudy() {
+		const role = await rolelookup();
+		return RequestApi({
+			method: 'PUT',
+			url: MemberSelfstudyController.cancelStudy(role),
+		});
+	}
+	async countstudy() {
+		const role = await rolelookup();
+		return RequestApi({
+			method: 'GET',
+			url: MemberSelfstudyController.countStudy(role),
+		});
+	}
+	async classlookup(classID: string) {
+		const role = await rolelookup();
+		return RequestApi({
+			method: 'GET',
+			url: MemberSelfstudyController.classLookup(parseInt(classID), role),
+		});
+	}
+	async studystatus() {
+		const role = await rolelookup();
+		return RequestApi({
+			method: 'GET',
+			url: MemberSelfstudyController.studyStatus(role),
+		});
 	}
 }
 
-export default new selfStudy();
+export default new SelfStudy();
