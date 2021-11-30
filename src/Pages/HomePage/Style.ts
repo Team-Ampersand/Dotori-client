f6b32fafd392a53ee995baf8adb253b71229104b
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-11-30 16:33:11 +0900

    Update style

--- src/Pages/HomePage/Style.ts
+++ src/Pages/HomePage/Style.ts
@@ -25,8 +25,6 @@ export const MealBoard = styled.div`
 export const Selfstudyboard = styled.div`
 	width: 35%;
 	height: 100%;
-	background-color: #fff;
-	border-radius: 20px;
 `;
 
 export const Container = styled.div`
