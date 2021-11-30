109bcb2f5a26646967c359d456406d4de9bdad59
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-11-30 19:52:21 +0900

    Update 레거시 삭제 및 style fix

--- src/Components/UserProfile/UserProfile.tsx
+++ src/Components/UserProfile/UserProfile.tsx
@@ -88,10 +88,6 @@ const UserProfile: React.FC = () => {
 					<sub>{profile?.point}</sub>
 				</S.PointWrapper>
 			</S.Content>
-			<S.Footer>
-				<S.Label>나의 이벤트</S.Label>
-				<EventList eventObject={SampleData} />
-			</S.Footer>
 			<S.Policy>
 				<span>© 2021 Ampersand. All Rights Reserved.</span>
 				<p>
