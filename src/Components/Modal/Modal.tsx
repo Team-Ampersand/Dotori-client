f8e6ec10714aa2fa91cb34785bb065894f8584f8
songsihyeon <s20040@gsm.hs.kr>
2021-11-30 15:58:47 +0900

    Add 공지사항 수정 api

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,38 +1,116 @@
-import React from 'react';
-import * as S from './Style';
+import notice from "Api/notice";
+import React, { useEffect, useState } from "react";
+import * as S from "./Style";
 
 interface ModalProps {
-	board_key: number;
-	modalState: boolean;
-	closeModal: (e: any) => void;
-	authorColor: string;
+  board_key: number;
+  modalState: boolean;
+  closeModal: (e: any) => void;
+  authorColor: string;
+  updateState: boolean;
+  setUpdateState: (e: any) => void;
 }
 
 const Modal: React.FC<ModalProps> = ({
-	board_key,
-	modalState,
-	closeModal,
-	authorColor,
+  board_key,
+  modalState,
+  closeModal,
+  authorColor,
+  updateState,
+  setUpdateState,
 }) => {
-	return modalState ? (
-		<div>
-			<S.Positioner>
-				<S.Overlay onClick={(e) => closeModal(e)} />
-				<S.Container>
-					<S.Title authorColor={authorColor}>
-						이건 테스트 제목입니다.
-						<S.Close onClick={(e) => closeModal(e)}>X</S.Close>
-					</S.Title>
-					<S.Content>
-						가나다라마 아에이오우 자음과 모음이 만나서 글자를 만들어요
-						가나다라마 아에이오우 그 글자들로 우린 얘기 해~
-					</S.Content>
-				</S.Container>
-			</S.Positioner>
-		</div>
-	) : (
-		<></>
-	);
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  const getNoticeItem = (boardId) => {
+    return notice.adminGetNoticeItem(boardId);
+  };
+
+  useEffect(() => {
+    getNoticeItem(board_key)
+      .then((res) => res.data.data)
+      .then((res) => {
+        setTitle(res.title);
+        setContent(res.content);
+        setUpdateTitle(res.title);
+        setUpdateContent(res.content);
+      });
+  }, []);
+
+  const [updateTitle, setUpdateTitle] = useState<string>("");
+  const [updateContent, setUpdateContent] = useState<string>("");
+
+  const onUpdate = async (e) => {
+    await notice.adminUpdateNotice(board_key, updateTitle, updateContent);
+    closeModal(e);
+    setUpdateState(false);
+    window.location.reload();
+  };
+
+  const updateClose = (e) => {
+    if (window.confirm("변경사항이 저장되지 않을 수 있습니다.")) {
+      closeModal(e);
+    }
+  };
+
+  return modalState ? (
+    <div>
+      <S.Positioner>
+        {updateState ? (
+          <S.Overlay onClick={updateClose} />
+        ) : (
+          <S.Overlay onClick={(e) => closeModal(e)} />
+        )}
+        <S.Container>
+          <S.Title authorColor={authorColor}>
+            {updateState ? (
+              <>
+                <input
+                  value={updateTitle}
+                  onChange={(e) => setUpdateTitle(e.target.value)}
+                />
+                <S.Close onClick={updateClose}>X</S.Close>
+              </>
+            ) : (
+              <>
+                {title}
+                <S.Close onClick={(e) => closeModal(e)}>X</S.Close>
+              </>
+            )}
+          </S.Title>
+          <S.Content>
+            {updateState ? (
+              <>
+                <textarea
+                  value={updateContent}
+                  onChange={(e) => setUpdateContent(e.target.value)}
+                />
+              </>
+            ) : (
+              <>
+                {content?.split("\n").map((line) => (
+                  <p>{line}</p>
+                ))}
+              </>
+            )}
+          </S.Content>
+          {updateState ? (
+            <>
+              <S.BtnWrapper color={authorColor}>
+                <S.Btn color={authorColor} onClick={onUpdate}>
+                  수정
+                </S.Btn>
+              </S.BtnWrapper>
+            </>
+          ) : (
+            <></>
+          )}
+        </S.Container>
+      </S.Positioner>
+    </div>
+  ) : (
+    <></>
+  );
 };
 
 export default Modal;
--- src/Utils/Libs/requestUrls.ts
+++ src/Utils/Libs/requestUrls.ts
@@ -1,48 +1,64 @@
 //멤버 관리
 export const MemberController = {
-	// 로그인
-	signin: () => {
-		return '/signin';
-	},
-	// 회원 가입
-	signup: () => {
-		return '/signup';
-	},
-	// 로그아웃
-	logout: () => {
-		return '/logout';
-	},
-	// 비밀번호 변경
-	change: () => {
-		return '/change/password';
-	},
-	// 회원탈퇴
-	delete: () => {
-		return '/delete';
-	},
+  // 로그인
+  signin: () => {
+    return "/signin";
+  },
+  // 회원 가입
+  signup: () => {
+    return "/signup";
+  },
+  // 로그아웃
+  logout: () => {
+    return "/logout";
+  },
+  // 비밀번호 변경
+  change: () => {
+    return "/change/password";
+  },
+  // 회원탈퇴
+  delete: () => {
+    return "/delete";
+  },
 };
 
 // 멤버 자습신청 관리
 export const selfStudyController = {
-	// 자습신청
-	apply: () => {
-		return '/member/selfstudy';
-	},
-	// 자습신청
-	cancel: () => {
-		return '/member/cancel/selfstudy';
-	},
+  // 자습신청
+  apply: () => {
+    return "/member/selfstudy";
+  },
+  // 자습신청
+  cancel: () => {
+    return "/member/cancel/selfstudy";
+  },
 };
 
 // 공지사항 관리
 export const noticeController = {
-	// admin
-	// admin 공지사항 조회
-	getNotice: () => {
-		return '/admin/board';
-	},
-	// admin 공지사항 작성
-	adminWrite: () => {
-		return '/admin/board';
-	},
+  // admin
+  // admin 공지사항 전체 조회
+  getNotice: () => {
+    return "/admin/board";
+  },
+  // admin 공지사항 페이지별 조회
+  getNoticeDetail: (page: number) => {
+    return `/admin/board?page=${page}`;
+  },
+  // admin 공지사항 작성
+  adminNoticeWrite: () => {
+    return "/admin/board";
+  },
+  // admin 공지사항 개별 조회
+  getNoticeItem: (boardId) => {
+    return `/admin/board/${boardId}`;
+  },
+  // admin 공지사항 삭제
+  deleteNotice: (boardId) => {
+    return `/admin/board/${boardId}`;
+  },
+  // 공지사항 수정
+  updateNotice: (boardId) => {
+    return `/admin/board/${boardId}`;
+  },
 };
