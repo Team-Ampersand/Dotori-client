6dfcc30817f29c80f4e1b522ad40650338f30436
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-11-30 11:08:56 +0900

    [Update] 안쓰는 코드 삭제 및 prettier 적용

--- src/Components/SongRequest/SongRequest.tsx
+++ src/Components/SongRequest/SongRequest.tsx
@@ -1,82 +1,19 @@
-import React, { useState } from "react";
-import axios from "axios";
-import * as S from "./Style";
+import React from 'react';
+import * as S from './Style';
 
 type SubmitFormProps = {
 	onSubmit: (form: { music: string; singer: string; link: string }) => void;
 };
 
 const SongRequest: React.FC<SubmitFormProps> = ({ onSubmit }) => {
-	// const [form, setForm] = useState({
-	// 	music: "",
-	// 	singer: "",
-	// 	link: "",
-	// });
-
-	// const { music, singer, link } = form;
-
-	//  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-	// 	const { name, value } = e.target;
-	// 	setForm({
-	// 		...form,
-	// 		[name]: value,
-	// 	});
-	// };
-
-	//const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-	// 	if (music.length < 1) {
-	// 		alert("음악제목을 입력하세요.");
-	// 	} else if (singer.length < 1) {
-	// 		alert("아티스트를 입력하세요.");
-	// 	} else if (link.length < 1) {
-	// 		alert("주소를 입력하세요.");
-	// 	} else if (!CheckUrl(link)) {
-	// 		alert("주소 형식이 올바르지 않습니다.");
-	// 	} else {
-	// 		e.preventDefault();
-	// 		onSubmit(form);
-	// 		axios
-	// 			.post("http://localhost:5000/api/music/v1", {
-	// 				user_id: 1,
-	// 				music: music,
-	// 				singer: singer,
-	// 				link: link,
-	// 			})
-	// 			.then((response) => {
-	// 				console.log(response);
-	// 				if (response.status === 201) {
-	// 					alert("음악 신청에 성공하였습니다!");
-	// 				} else if (response.status === 400) {
-	// 					alert("유저 토큰 인증 과정 중 오류 발생");
-	// 				} else {
-	// 					alert("Error");
-	// 				}
-	// 			})
-	// 			.catch((response) => {
-	// 				console.log(response);
-	// 			});
-	// 		setForm({
-	// 			music: "",
-	// 			singer: "",
-	// 			link: "",
-	// 		});
-	// 	}
-	// };
-
 	return (
 		<S.Postioner>
 			<label>기상음악 신청</label>
-			<form>
-				<S.InputWrapper>
-					<p>URL 주소</p>
-					<input
-						name="link"
-						placeholder="url을 입력하세요."
-						type="text"
-					/>
-					<button>신청하기</button>
-				</S.InputWrapper>
-			</form>
+			<S.InputWrapper>
+				<p>URL 주소</p>
+				<input name="link" placeholder="url을 입력하세요." type="text" />
+				<button>신청하기</button>
+			</S.InputWrapper>
 		</S.Postioner>
 	);
 };
