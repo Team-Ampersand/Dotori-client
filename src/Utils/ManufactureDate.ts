d30cf36fd9f2a29df0668ef40f3ac56766ec902a
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-12-08 20:08:51 +0900

    Update 급식 api 연동

--- src/Utils/ManufactureDate.ts
+++ src/Utils/ManufactureDate.ts
@@ -1,10 +1,21 @@
-export function ManufactureDate(dateType : 'Y' | 'M' | 'D' | 'W') {
-  var week = ['일', '월', '화', '수', '목', '금', '토'];
-  switch (dateType) {
-      case 'Y': return new Date().getFullYear();
-      case 'M': return new Date().getMonth() + 1;
-      case 'D': return new Date().getDate();
-      case 'W': return week[new Date().getDay()];
-      default: return false;
-  }
+export function ManufactureDate(dateType: 'Y' | 'M' | 'D' | 'W' | 'YMD') {
+	var week = ['일', '월', '화', '수', '목', '금', '토'];
+	switch (dateType) {
+		case 'Y':
+			return new Date().getFullYear();
+		case 'M':
+			return new Date().getMonth() + 1;
+		case 'D':
+			return new Date().getDate();
+		case 'W':
+			return week[new Date().getDay()];
+		case 'YMD':
+			return (
+				new Date().getFullYear() +
+				('0' + (new Date().getMonth() + 1)).slice(-2) +
+				('0' + new Date().getDate()).slice(-2)
+			);
+		default:
+			return false;
+	}
 }
