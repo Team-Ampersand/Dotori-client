76bc81bfd3eff733ca9db89e6ae55aad48b1999f
songsihyeon <s20040@gsm.hs.kr>
2021-12-17 16:10:30 +0900

    Add stu role change 개발자 권한

--- src/Components/StuAuthorityItem/StuAuthorityItem.tsx
+++ src/Components/StuAuthorityItem/StuAuthorityItem.tsx
@@ -14,7 +14,7 @@ const returnRoleValue = (roleType: string) => {
     case "ROLE_COUNCILLOR":
       return "자치위원회";
     case "ROLE_DEVELOPER":
-      return "도토리";
+      return "개발자";
     case "ROLE_MEMBER":
       return "학생";
     default:
@@ -41,28 +41,12 @@ const StuAuthorityItem: React.FC<StuAuthorityItemProps> = ({
     <>
       <S.Container>
         <S.StuInfoWrapper>
-          <S.StuNumStyle>
-            {/* <ChangeInput init={stuNum} edit={editState} type="number" /> */}
-            {stuNum}
-          </S.StuNumStyle>
-          <S.NameStyle>
-            {/* <ChangeInput init={name} edit={editState} type="text" /> */}
-            {name}
-          </S.NameStyle>
-          <S.AuthorityStyle>
-            {/* <ChangeSelect init={returnRoleValue(authority)} edit={editState} /> */}
-            {returnRoleValue(authority)}
-          </S.AuthorityStyle>
+          <S.StuNumStyle>{stuNum}</S.StuNumStyle>
+          <S.NameStyle>{name}</S.NameStyle>
+          <S.AuthorityStyle>{returnRoleValue(authority)}</S.AuthorityStyle>
         </S.StuInfoWrapper>
         <S.EditBtn onClick={() => setEditState(!editState)}>수정</S.EditBtn>
-        {/* <S.BtnWrapper edit={false}>
-          <S.Btn BtnColor="green" onClick={onConfirm}>
-            완료
-          </S.Btn>
-          <S.Btn BtnColor="red" onClick={() => setEditState(!editState)}>
-            취소
-          </S.Btn>
-        </S.BtnWrapper> */}
+
         <InfoUpdateModal
           modalState={editState}
           closeModal={closeModal}
