7c76a5a7e87da032314e2e5e22d5e618550888a2
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-12-21 17:06:53 +0900

    Update manufacture date string타입으로 변환

--- src/Utils/ManufactureDate.ts
+++ src/Utils/ManufactureDate.ts
@@ -2,20 +2,20 @@ export function ManufactureDate(dateType: 'Y' | 'M' | 'D' | 'W' | 'YMD') {
 	var week = ['일', '월', '화', '수', '목', '금', '토'];
 	switch (dateType) {
 		case 'Y':
-			return new Date().getFullYear();
+			return `${new Date().getFullYear()}`;
 		case 'M':
-			return new Date().getMonth() + 1;
+			return `${new Date().getMonth() + 1}`;
 		case 'D':
-			return new Date().getDate();
+			return `${new Date().getDate()}`;
 		case 'W':
-			return week[new Date().getDay()];
+			return `${week[new Date().getDay()]}`;
 		case 'YMD':
-			return (
+			return `${
 				new Date().getFullYear() +
 				('0' + (new Date().getMonth() + 1)).slice(-2) +
 				('0' + new Date().getDate()).slice(-2)
-			);
+			}`;
 		default:
-			return false;
+			return '';
 	}
 }
