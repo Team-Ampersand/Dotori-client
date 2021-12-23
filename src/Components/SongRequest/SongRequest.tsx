ca2ed5ba4d21936eae4d5f8b16419b1481413474
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-12-23 17:26:43 +0900

    Update 신청불가 버튼, 조건 추가 및 렌더시 focus

--- src/Components/SongRequest/SongRequest.tsx
+++ src/Components/SongRequest/SongRequest.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as S from './Style';
 import music from 'Api/music';
 import { getCookie } from 'Utils/Cookie';
+import { ManufactureDate } from 'Utils/ManufactureDate';
 
 const musicApply = async (musicUrl: string) => {
 	try {
@@ -14,7 +15,14 @@ const musicApply = async (musicUrl: string) => {
 	}
 };
 
-const returnBtn = (url: string, setUrl) => {
+const returnBtn = (
+	url: string,
+	setUrl: { (value: React.SetStateAction<string>): void; (arg0: string): void },
+	songInput: React.LegacyRef<HTMLInputElement> | undefined
+) => {
+	const today = ManufactureDate('W');
+	let can = ['월', '화', '수', '목', '일'];
+	let cant = ['금', '토'];
 	const role = getCookie('role');
 	if (role === 'admin') {
 		return (
@@ -29,6 +37,19 @@ const returnBtn = (url: string, setUrl) => {
 				<p>사감 선생님은 노래신청을 하지 않으셔도 됩니다</p>
 			</>
 		);
+	} else if (cant.indexOf(today) !== -1) {
+		console.log(cant.indexOf(today));
+		return (
+			<>
+				<input
+					name="link"
+					placeholder="url을 입력하세요."
+					type="text"
+					disabled
+				/>
+				<button>신청불가</button>
+			</>
+		);
 	} else {
 		return (
 			<>
@@ -37,9 +58,12 @@ const returnBtn = (url: string, setUrl) => {
 					placeholder="url을 입력하세요."
 					type="text"
 					onChange={({ target: { value } }) => setUrl(value)}
+					ref={songInput}
 					onKeyPress={(e) => {
 						if (e.key === 'Enter') {
-							if (CheckUrl(url)) {
+							if (url === '') {
+								alert('아무것도 입력하지 않았습니다.');
+							} else if (CheckUrl(url)) {
 								musicApply(url).then(() => {
 									window.location.reload();
 								});
@@ -51,7 +75,9 @@ const returnBtn = (url: string, setUrl) => {
 				/>
 				<button
 					onClick={() => {
-						if (CheckUrl(url)) {
+						if (url === '') {
+							alert('아무것도 입력하지 않았습니다.');
+						} else if (CheckUrl(url)) {
 							musicApply(url).then(() => {
 								window.location.reload();
 							});
@@ -70,12 +96,20 @@ const returnBtn = (url: string, setUrl) => {
 const SongRequest: React.FC = () => {
 	const [url, setUrl] = useState<string>('');
 
+	const songInput = useRef<HTMLInputElement>(null);
+
+	useEffect(() => {
+		if (songInput.current) {
+			songInput.current.focus();
+		}
+	});
+
 	return (
 		<S.Postioner>
 			<label>기상음악 신청</label>
 			<S.InputWrapper>
 				<p>URL 주소</p>
-				{returnBtn(url, setUrl)}
+				{returnBtn(url, setUrl, songInput)}
 			</S.InputWrapper>
 		</S.Postioner>
 	);
