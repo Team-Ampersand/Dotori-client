8133ee962fb1ab54d24c7c19258dbc2ca2088f2e
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-01-01 15:40:10 +0900

    Update 권한 오류 로그아웃 및 리다이렉션

--- src/Components/SelfstudyTable/List/List.tsx
+++ src/Components/SelfstudyTable/List/List.tsx
@@ -27,6 +27,20 @@ const ReturnUserObj = async (history, setLogged) => {
 
 			setLogged(false);
 			window.location.reload();
+		} else if (e.message === 'Request failed with status code 403') {
+			alert('로그아웃 되었습니다. 다시 로그인 해주세요.');
+
+			// deleteCookie('Dotori_accessToken');
+			// deleteCookie('Dotori_refreshToken');
+			// deleteCookie('role');
+
+			localStorage.removeItem('Dotori_accessToken');
+			localStorage.removeItem('Dotori_refreshToken');
+			localStorage.removeItem('role');
+
+			history.push('/');
+			setLogged(false);
+			window.location.reload();
 		}
 	}
 };
