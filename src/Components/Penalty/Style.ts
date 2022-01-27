54c960dde3f8470ff00202f90ab59dc7907c37a6
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2022-01-27 22:23:36 +0900

    수정

--- src/Components/Penalty/Style.ts
+++ src/Components/Penalty/Style.ts
@@ -71,7 +71,7 @@ export const AuthorizationBoard = styled.ul`
 export const Container = styled.div`
   display: flex;
   width: 100%;
-  height: 70%;
+  height: 75vh;
   background-color: #FFFFFF;
   border-radius: 20px;
 `;
