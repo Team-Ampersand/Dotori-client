6c8213d6f2960efdc7becee106f46921101bade7
songsihyeon <s20040@gsm.hs.kr>
2022-02-09 22:52:01 +0900

    Add search name

--- src/Components/Authorzation/Authorzation.tsx
+++ src/Components/Authorzation/Authorzation.tsx
@@ -24,8 +24,9 @@ const Authorzation: React.FC = () => {
     return await stuInfo.getClassStuInfo(role, parseInt(stuGrade + stuClass));
   };
 
-  const [stuGrade, setStuGrade] = useState("");
-  const [stuClass, setStuClass] = useState("");
+  const [stuGrade, setStuGrade] = useState<string>("");
+  const [stuClass, setStuClass] = useState<string>("");
+  const [stuName, setStuName] = useState<string>("");
 
   const onSubmit = () => {
     if (parseInt(stuGrade + stuClass) > 0) {
@@ -37,6 +38,17 @@ const Authorzation: React.FC = () => {
         throw Error(e);
       }
     } else return;
+  };
+
+  const nameSearch = async () => {
+    const role = localStorage.getItem("role");
+    return await stuInfo.searchName(role, stuName);
+  };
+
+  const onSearch = () => {
+    nameSearch().then((res) => {
+      res && setStudentList(res.data.data);
+    });
   };
 
   useEffect(() => {
@@ -51,38 +63,49 @@ const Authorzation: React.FC = () => {
 
   return (
     <S.Positioner>
-      <S.SelectWrapper>
-        <S.SelectBox
-          onChange={({ target: { value } }) => {
-            setStuGrade(value);
-          }}
-          value={stuGrade}
-        >
-          <S.Option value="" selected disabled hidden>
-            선택
-          </S.Option>
-          <S.Option value="1">1</S.Option>
-          <S.Option value="2">2</S.Option>
-          <S.Option value="3">3</S.Option>
-        </S.SelectBox>
-        <S.Label>학년</S.Label>
-        <S.SelectBox
-          onChange={({ target: { value } }) => {
-            setStuClass(value);
-          }}
-          value={stuClass}
-        >
-          <S.Option value="" selected disabled hidden>
-            선택
-          </S.Option>
-          <S.Option value="1">1</S.Option>
-          <S.Option value="2">2</S.Option>
-          <S.Option value="3">3</S.Option>
-          <S.Option value="4">4</S.Option>
-        </S.SelectBox>
-        <S.Label>반</S.Label>
-        <S.Btn onClick={onSubmit}>검색</S.Btn>
-      </S.SelectWrapper>
+      <div>
+        <S.SelectWrapper>
+          <S.SelectBox
+            onChange={({ target: { value } }) => {
+              setStuGrade(value);
+            }}
+            value={stuGrade}
+          >
+            <S.Option value="" selected disabled hidden>
+              선택
+            </S.Option>
+            <S.Option value="1">1</S.Option>
+            <S.Option value="2">2</S.Option>
+            <S.Option value="3">3</S.Option>
+          </S.SelectBox>
+          <S.Label>학년</S.Label>
+          <S.SelectBox
+            onChange={({ target: { value } }) => {
+              setStuClass(value);
+            }}
+            value={stuClass}
+          >
+            <S.Option value="" selected disabled hidden>
+              선택
+            </S.Option>
+            <S.Option value="1">1</S.Option>
+            <S.Option value="2">2</S.Option>
+            <S.Option value="3">3</S.Option>
+            <S.Option value="4">4</S.Option>
+          </S.SelectBox>
+          <S.Label>반</S.Label>
+          <S.Btn onClick={onSubmit}>검색</S.Btn>
+        </S.SelectWrapper>
+        <S.SearchBox>
+          <S.Search
+            placeholder="이름으로 검색하기"
+            onChange={(e) => {
+              setStuName(e.target.value);
+            }}
+          />
+          <S.SearchBtn onClick={onSearch}>검색</S.SearchBtn>
+        </S.SearchBox>
+      </div>
       <S.AuthorizationBoard>
         {studentList &&
           studentList.map((stu) => (
