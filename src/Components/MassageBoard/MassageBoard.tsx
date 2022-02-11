ed5c21aeea26d24f2a08548057af3731e9850861
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-02-12 00:22:22 +0900

    Add Massage 기능 추가

---
+++ src/Components/MassageBoard/MassageBoard.tsx
@@ -0,0 +1,143 @@
+import massage from 'Api/massage';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { ManufactureDate } from 'Utils/ManufactureDate';
+import * as S from './Style';
+
+const massageInfo = async () => {
+	const res = await massage.massageInfo();
+	return res;
+};
+
+const returnRoomStatusNumber = (compareMax: number, compareMin: number) => {
+	if (compareMax / 2 > compareMin) {
+		return '#28C842';
+	} else if (compareMax * (7 / 8) > compareMin) {
+		return '#F8C600';
+	} else {
+		return '#E11A1A';
+	}
+};
+
+const applyMassage = async (setInfo, count: number) => {
+	await massage.massage();
+	setInfo({ count: count + 1, status: 'APPLIED' });
+	alert('안마의자 신청이 완료되었습니다.');
+};
+
+const cancelMassage = async (setInfo, count: number) => {
+	await massage.cancelMassage();
+	setInfo({ count: count - 1, status: 'CANT' });
+	alert(
+		'안마의자 신청이 취소 되었습니다. 오늘 하루 동안 다시 신청이 불가능 합니다.'
+	);
+};
+
+const returnButton = (
+	status: string,
+	setInfo: React.Dispatch<
+		React.SetStateAction<{ count: string; status: string }>
+	>,
+	count: number
+) => {
+	let today: string = ManufactureDate('W');
+	let can = ['월', '화', '수', '목'];
+	let cant = ['금', '토', '일'];
+	let hours = new Date().getHours();
+	let minutes = new Date().getMinutes();
+	if (localStorage.getItem('role') === 'admin') {
+		return <p>사감 선생님은 안마의자 신청을 하지 않으셔도 됩니다.</p>;
+	} else if (
+		status === 'CAN' &&
+		can.indexOf(today) !== -1 &&
+		hours >= 20 &&
+		hours < 21 &&
+		minutes >= 20
+	) {
+		return (
+			<S.MassageButton
+				onClick={() => {
+					applyMassage(setInfo, count);
+				}}
+				status={status}
+			>
+				안마의자 신청
+			</S.MassageButton>
+		);
+	} else if (status === 'APPLIED') {
+		return (
+			<S.MassageButton
+				onClick={() => {
+					if (window.confirm('안마의자 신청을 취소하시겠습니까?')) {
+						cancelMassage(setInfo, count);
+					} else alert('안마의자 신청이 취소되지 않았습니다.');
+				}}
+				status={status}
+			>
+				안마의자 취소
+			</S.MassageButton>
+		);
+	} else if (
+		status === 'CANT' ||
+		count >= 50 ||
+		cant.indexOf(today) !== -1 ||
+		hours < 20 ||
+		hours >= 21 ||
+		minutes < 20
+	) {
+		return (
+			<S.MassageButton
+				onClick={() => alert('안마의자 신청이 불가능합니다.')}
+				status={status}
+			>
+				신청불가
+			</S.MassageButton>
+		);
+	} else {
+		<S.MassageButton
+			onClick={() => alert('안마의자 신청이 불가능합니다.')}
+			status={status}
+		>
+			신청불가
+		</S.MassageButton>;
+	}
+};
+
+const MassageBoard: React.FC = () => {
+	const [info, setInfo] = useState({ count: '0', status: '' });
+
+	useEffect(() => {
+		massageInfo().then((res) => {
+			setInfo(res?.data.data);
+		});
+	}, []);
+
+	return (
+		<S.Positioner status={info.status}>
+			<S.MassageHeader>
+				<h2>안마의자 신청</h2>
+				<div>
+					<Link to="/massage">
+						<span>신청 현황</span>
+					</Link>
+				</div>
+			</S.MassageHeader>
+			<S.MassageContent>
+				<strong>
+					{ManufactureDate('Y')}년 {ManufactureDate('M')}월{' '}
+					{ManufactureDate('D')}일 {ManufactureDate('W')}요일
+				</strong>
+				<span>{info.count}/5</span>
+				<S.PointProgress>
+					<S.ActiveProgress
+						statusColor={returnRoomStatusNumber(5, parseInt(info.count))}
+						count={parseInt(info.count)}
+					/>
+				</S.PointProgress>
+				{returnButton(info.status, setInfo, parseInt(info.count))}
+			</S.MassageContent>
+		</S.Positioner>
+	);
+};
+
+export default React.memo(MassageBoard);
---
+++ src/Components/MassageBoard/Style.ts
@@ -0,0 +1,172 @@
+import styled from 'styled-components';
+import { ManufactureDate } from 'Utils/ManufactureDate';
+
+type StyleProps = {
+	statusColor?: string;
+	status?: string;
+	count?: number;
+};
+
+export const Positioner = styled.div<StyleProps>`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	border: 2px solid
+		${(props) => {
+			if (
+				props.status === 'CAN' &&
+				new Date().getHours() >= 20 &&
+				new Date().getHours() < 21 &&
+				['월', '화', '수', '목'].indexOf(ManufactureDate('W')) !== -1 &&
+				new Date().getMinutes() >= 20
+			) {
+				return '#fff';
+			} else if (props.status === 'APPLIED') {
+				return '#617be3';
+			} else if (
+				props.status === 'CANT' ||
+				new Date().getHours() < 20 ||
+				new Date().getHours() >= 21 ||
+				['금', '토', '일'].indexOf(ManufactureDate('W')) !== -1 ||
+				new Date().getMinutes() < 20
+			) {
+				return '#fc2c2c';
+			}
+		}};
+	background-color: #fff;
+	border-radius: 20px;
+`;
+
+export const MassageHeader = styled.div`
+	height: 60px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 0 30px;
+
+	h2 {
+		font-size: 20px;
+	}
+	div {
+		display: flex;
+		align-items: center;
+		cursor: pointer;
+		a {
+			text-decoration: none;
+			span {
+				font-size: 18px;
+				color: #8b8b8b;
+			}
+		}
+	}
+`;
+
+export const MassageContent = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	justify-content: space-evenly;
+	strong {
+		font-size: 18px;
+	}
+	span {
+		font-size: 5em;
+		font-weight: bold;
+	}
+	p {
+		font-size: 18px;
+		font-weight: 500;
+	}
+`;
+
+export const PointProgress = styled.div`
+	position: relative;
+	width: 60%;
+	height: 10px;
+	border-radius: 20px;
+	background-color: #ededed;
+`;
+
+export const ActiveProgress = styled.div<StyleProps>`
+	position: absolute;
+	width: ${(props) => props.count! * 20}%;
+	height: 9px;
+	border-radius: 20px;
+	background-color: ${(props) => props.statusColor};
+`;
+
+export const MassageButton = styled.button<StyleProps>`
+	width: 40%;
+	height: 20%;
+	border: none;
+	background-color: ${(props) => {
+		if (
+			props.status === 'CAN' &&
+			new Date().getHours() >= 20 &&
+			new Date().getHours() < 21 &&
+			['월', '화', '수', '목'].indexOf(ManufactureDate('W')) !== -1 &&
+			new Date().getMinutes() >= 20
+		) {
+			return '#617be3';
+		} else if (props.status === 'APPLIED') {
+			return '#fff';
+		} else if (
+			props.status === 'CANT' ||
+			new Date().getHours() < 20 ||
+			new Date().getHours() >= 21 ||
+			['금', '토', '일'].indexOf(ManufactureDate('W')) !== -1 ||
+			new Date().getMinutes() < 20
+		) {
+			return '#fc2c2c';
+		}
+	}};
+	color: ${(props) => {
+		if (
+			props.status === 'CAN' &&
+			new Date().getHours() >= 20 &&
+			new Date().getHours() < 21 &&
+			['월', '화', '수', '목'].indexOf(ManufactureDate('W')) !== -1 &&
+			new Date().getMinutes() >= 20
+		) {
+			return '#fff';
+		} else if (props.status === 'APPLIED') {
+			return '#617be3';
+		} else if (
+			props.status === 'CANT' ||
+			new Date().getHours() < 20 ||
+			new Date().getHours() >= 21 ||
+			['금', '토', '일'].indexOf(ManufactureDate('W')) !== -1 ||
+			new Date().getMinutes() < 20
+		) {
+			return '#fff';
+		}
+	}};
+	border: 2px solid
+		${(props) => {
+			if (
+				props.status === 'CAN' &&
+				new Date().getHours() >= 20 &&
+				new Date().getHours() < 21 &&
+				['월', '화', '수', '목'].indexOf(ManufactureDate('W')) !== -1 &&
+				new Date().getMinutes() >= 20
+			) {
+				return '#fff';
+			} else if (props.status === 'APPLIED') {
+				return '#617be3';
+			} else if (
+				props.status === 'CANT' ||
+				new Date().getHours() < 20 ||
+				new Date().getHours() >= 21 ||
+				['금', '토', '일'].indexOf(ManufactureDate('W')) !== -1 ||
+				new Date().getMinutes() < 20
+			) {
+				return '#fff';
+			}
+		}};
+	border-radius: 10px;
+	font-size: 20px;
+	font-weight: bold;
+`;
