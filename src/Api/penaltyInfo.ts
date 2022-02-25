273abaf73eb557b0c4f67cc8419284ec48a19be1
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-02-25 23:36:36 +0900

    Update MainPage 규정위반 내역 조회 추가

--- src/Api/penaltyInfo.ts
+++ src/Api/penaltyInfo.ts
@@ -1,55 +1,65 @@
-import { PenaltyController } from "Utils/Libs/requestUrls";
-import RequestApi from "Utils/Libs/requestApi";
+import { PenaltyController } from 'Utils/Libs/requestUrls';
+import RequestApi from 'Utils/Libs/requestApi';
 
 class penaltyInfo {
-  async getPenaltyInfo(role: string | null, stuNum: string){
-    try{
-      return await RequestApi({
-        method: "GET",
-        url: PenaltyController.getPenaltyInfo(role, stuNum),
-      });
-    } catch (e: any) {
-      if (e.message === "Request failed with status code 404") {
-        alert("1");
-      } else return;
-    }
-  }
-  updatePenaltyInfo(date: string, rule: string, stuNum: Array<string>){
-    try{
-      const data = {
-        date: date,
-        rule: rule,
-        stuNum: stuNum,
-      };
-      return RequestApi({
-        method: "POST",
-        url: PenaltyController.updatePenaltyInfo(localStorage.getItem("role")),
-        data: data,
-      });
-    } catch (e: any) {
-      throw new Error(e);
-    }
-  }
-  async getModifyPenaltyInfo(role: string | null, stuNum) {
-    try{
-      return await RequestApi({
-        method: "GET",
-        url: PenaltyController.getModifyPenaltyInfo(role, stuNum),
-      });
-    } catch (e: any) {
-      throw new Error(e);
-    }
-  }
-  async deletePenaltyInfo(role: string | null, PenaltyId: number){
-    try{
-      return await RequestApi({
-        method: "DELETE",
-        url: PenaltyController.deletePenaltyInfo(role, PenaltyId),
-      });
-    } catch (e: any) {
-      throw new Error(e);
-    }
-  }
+	async getPenaltyInfo(role: string | null, stuNum: string) {
+		try {
+			return await RequestApi({
+				method: 'GET',
+				url: PenaltyController.getPenaltyInfo(role, stuNum),
+			});
+		} catch (e: any) {
+			if (e.message === 'Request failed with status code 404') {
+				alert('1');
+			} else return;
+		}
+	}
+	updatePenaltyInfo(date: string, rule: string, stuNum: Array<string>) {
+		try {
+			const data = {
+				date: date,
+				rule: rule,
+				stuNum: stuNum,
+			};
+			return RequestApi({
+				method: 'POST',
+				url: PenaltyController.updatePenaltyInfo(localStorage.getItem('role')),
+				data: data,
+			});
+		} catch (e: any) {
+			throw new Error(e);
+		}
+	}
+	getModifyPenaltyInfo(role: string | null, stuNum: any) {
+		try {
+			return RequestApi({
+				method: 'GET',
+				url: PenaltyController.getModifyPenaltyInfo(role, stuNum),
+			});
+		} catch (e: any) {
+			throw new Error(e);
+		}
+	}
+	async deletePenaltyInfo(role: string | null, PenaltyId: number) {
+		try {
+			return await RequestApi({
+				method: 'DELETE',
+				url: PenaltyController.deletePenaltyInfo(role, PenaltyId),
+			});
+		} catch (e: any) {
+			throw new Error(e);
+		}
+	}
+	getPenaltyMainInfo() {
+		try {
+			return RequestApi({
+				method: 'GET',
+				url: PenaltyController.getPenaltyMainInfo(localStorage.getItem('role')),
+			});
+		} catch (e: any) {
+			throw new Error(e);
+		}
+	}
 }
 
 export default new penaltyInfo();
