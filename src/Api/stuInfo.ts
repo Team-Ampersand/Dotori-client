060a10cc5500c88f5b1059779e0da720a84d74d5
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-02-25 23:38:18 +0900

    Update name 혹은 userName에서 memberName으로 변경

--- src/Api/stuInfo.ts
+++ src/Api/stuInfo.ts
@@ -1,123 +1,123 @@
-import { StuInfoController } from "Utils/Libs/requestUrls";
-import RequestApi from "Utils/Libs/requestApi";
+import { StuInfoController } from 'Utils/Libs/requestUrls';
+import RequestApi from 'Utils/Libs/requestApi';
 
 class stuInfo {
-  async getStuInfo(role: string | null) {
-    try {
-      return await RequestApi({
-        method: "GET",
-        url: StuInfoController.getStuInfo(role),
-      });
-    } catch (e: any) {
-      if (e.massage === "Request failed with status code 404") {
-        alert("등록된 학생 정보가 없습니다.");
-      } else if (e.massage === "Request failed with status code 401") {
-        alert("로그아웃 되었습니다. 다시 로그인 해주세요.");
-        localStorage.removeItem("Dotori_accessToken");
-        localStorage.removeItem("Dotori_refreshToken");
-        localStorage.removeItem("role");
-        window.location.reload();
-      } else throw Error(e);
-    }
-  }
-  async getClassStuInfo(role: string | null, classId: number) {
-    try {
-      return await RequestApi({
-        method: "GET",
-        url: StuInfoController.getClassStuInfo(role, classId),
-      });
-    } catch (e: any) {
-      if (e.message === "Request failed with status code 404") {
-        alert("해당 반에 등록된 학생 정보가 없습니다.");
-      } else return;
-    }
-  }
-  async updateStuNum(role: string | null, stuId: number, stuNum: string) {
-    try {
-      const data = {
-        receiverId: stuId,
-        stuNum: stuNum,
-      };
-      return await RequestApi({
-        method: "PUT",
-        url: StuInfoController.updateStuNum(role),
-        data: data,
-      });
-    } catch (e: any) {
-      if (e.message === "Request failed with status code 409") {
-        return;
-      } else throw Error(e);
-    }
-  }
-  async updateStuName(role: string | null, stuId: number, stuName: string) {
-    try {
-      const data = {
-        receiverId: stuId,
-        username: stuName,
-      };
-      return await RequestApi({
-        method: "PUT",
-        url: StuInfoController.updateStuName(role),
-        data: data,
-      });
-    } catch (e: any) {
-      if (e.message === "Request failed with status code 409") {
-        return;
-      } else throw Error(e);
-    }
-  }
-  async updateStuRole(role: string | null, stuId: number, stuRole: string) {
-    try {
-      const data = {
-        receiverId: stuId,
-        roles: [stuRole],
-      };
-      return await RequestApi({
-        method: "PUT",
-        url: StuInfoController.updateStuRole(role),
-        data: data,
-      });
-    } catch (e: any) {
-      if (e.message === "Request failed with status code 409") {
-        return;
-      } else throw Error(e);
-    }
-  }
-  async banSelfStudy(role: string | null, stuId: number) {
-    try {
-      return await RequestApi({
-        method: "PUT",
-        url: StuInfoController.banSelfStudy(role, stuId),
-      });
-    } catch (e: any) {
-      throw Error(e);
-    }
-  }
-  async banCancelSelfStudy(role: string | null, stuId: number) {
-    try {
-      return await RequestApi({
-        method: "PUT",
-        url: StuInfoController.banCancelSelfStudy(role, stuId),
-      });
-    } catch (e: any) {
-      throw Error(e);
-    }
-  }
-  async searchName(role: string | null, name: string) {
-    try {
-      if (name === "") {
-        return await this.getStuInfo(role);
-      }
-      return await RequestApi({
-        method: "GET",
-        url: StuInfoController.searchName(role, name),
-      });
-    } catch (e: any) {
-      if (e.message === "Request failed with status code 404") {
-        alert("해당하는 학생이 없습니다.");
-      } else throw Error(e);
-    }
-  }
+	async getStuInfo(role: string | null) {
+		try {
+			return await RequestApi({
+				method: 'GET',
+				url: StuInfoController.getStuInfo(role),
+			});
+		} catch (e: any) {
+			if (e.massage === 'Request failed with status code 404') {
+				alert('등록된 학생 정보가 없습니다.');
+			} else if (e.massage === 'Request failed with status code 401') {
+				alert('로그아웃 되었습니다. 다시 로그인 해주세요.');
+				localStorage.removeItem('Dotori_accessToken');
+				localStorage.removeItem('Dotori_refreshToken');
+				localStorage.removeItem('role');
+				window.location.reload();
+			} else throw Error(e);
+		}
+	}
+	async getClassStuInfo(role: string | null, classId: number) {
+		try {
+			return await RequestApi({
+				method: 'GET',
+				url: StuInfoController.getClassStuInfo(role, classId),
+			});
+		} catch (e: any) {
+			if (e.message === 'Request failed with status code 404') {
+				alert('해당 반에 등록된 학생 정보가 없습니다.');
+			} else return;
+		}
+	}
+	async updateStuNum(role: string | null, stuId: number, stuNum: string) {
+		try {
+			const data = {
+				receiverId: stuId,
+				stuNum: stuNum,
+			};
+			return await RequestApi({
+				method: 'PUT',
+				url: StuInfoController.updateStuNum(role),
+				data: data,
+			});
+		} catch (e: any) {
+			if (e.message === 'Request failed with status code 409') {
+				return;
+			} else throw Error(e);
+		}
+	}
+	async updateStuName(role: string | null, stuId: number, memberName: string) {
+		try {
+			const data = {
+				receiverId: stuId,
+				memberName: memberName,
+			};
+			return await RequestApi({
+				method: 'PUT',
+				url: StuInfoController.updateStuName(role),
+				data: data,
+			});
+		} catch (e: any) {
+			if (e.message === 'Request failed with status code 409') {
+				return;
+			} else throw Error(e);
+		}
+	}
+	async updateStuRole(role: string | null, stuId: number, stuRole: string) {
+		try {
+			const data = {
+				receiverId: stuId,
+				roles: [stuRole],
+			};
+			return await RequestApi({
+				method: 'PUT',
+				url: StuInfoController.updateStuRole(role),
+				data: data,
+			});
+		} catch (e: any) {
+			if (e.message === 'Request failed with status code 409') {
+				return;
+			} else throw Error(e);
+		}
+	}
+	async banSelfStudy(role: string | null, stuId: number) {
+		try {
+			return await RequestApi({
+				method: 'PUT',
+				url: StuInfoController.banSelfStudy(role, stuId),
+			});
+		} catch (e: any) {
+			throw Error(e);
+		}
+	}
+	async banCancelSelfStudy(role: string | null, stuId: number) {
+		try {
+			return await RequestApi({
+				method: 'PUT',
+				url: StuInfoController.banCancelSelfStudy(role, stuId),
+			});
+		} catch (e: any) {
+			throw Error(e);
+		}
+	}
+	async searchName(role: string | null, memberName: string) {
+		try {
+			if (memberName === '') {
+				return await this.getStuInfo(role);
+			}
+			return await RequestApi({
+				method: 'GET',
+				url: StuInfoController.searchName(role, memberName),
+			});
+		} catch (e: any) {
+			if (e.message === 'Request failed with status code 404') {
+				alert('해당하는 학생이 없습니다.');
+			} else throw Error(e);
+		}
+	}
 }
 
 export default new stuInfo();
--- src/Components/InfoUpdateModal/InfoUpdateModal.tsx
+++ src/Components/InfoUpdateModal/InfoUpdateModal.tsx
@@ -1,114 +1,114 @@
-import { useEffect, useState } from "react";
-import * as S from "./Style";
-import stuInfo from "../../Api/stuInfo";
+import { useEffect, useState } from 'react';
+import * as S from './Style';
+import stuInfo from '../../Api/stuInfo';
 
 interface ModalProps {
-  modalState: boolean;
-  closeModal: () => void;
-  stuNum: string;
-  name: string;
-  role: string;
-  stuId: number;
+	modalState: boolean;
+	closeModal: () => void;
+	stuNum: string;
+	memberName: string;
+	role: string;
+	stuId: number;
 }
 
 const InfoUpdateModal: React.FC<ModalProps> = ({
-  modalState,
-  closeModal,
-  stuNum,
-  name,
-  role,
-  stuId,
+	modalState,
+	closeModal,
+	stuNum,
+	memberName,
+	role,
+	stuId,
 }) => {
-  const [updateStuNum, setUpdateStuNum] = useState(stuNum);
-  const [updateName, setUpdateName] = useState(name);
-  const [updateRole, setUpdateRole] = useState(role);
+	const [updateStuNum, setUpdateStuNum] = useState(stuNum);
+	const [updateName, setUpdateName] = useState(memberName);
+	const [updateRole, setUpdateRole] = useState(role);
 
-  useEffect(() => {
-    window.history.pushState({ page: "modal" }, document.title);
-    window.addEventListener("popstate", closeModal);
-  });
+	useEffect(() => {
+		window.history.pushState({ page: 'modal' }, document.title);
+		window.addEventListener('popstate', closeModal);
+	});
 
-  const returnRoleValue = (role) => {
-    if (role === "자치위원") return "ROLE_COUNCILLOR";
-    if (role === "개발자") return "ROLE_DEVELOPER";
-    return "ROLE_MEMBER";
-  };
+	const returnRoleValue = (role) => {
+		if (role === '자치위원') return 'ROLE_COUNCILLOR';
+		if (role === '개발자') return 'ROLE_DEVELOPER';
+		return 'ROLE_MEMBER';
+	};
 
-  const stuNumUpdate = async () => {
-    const role = await localStorage.getItem("role");
-    await stuInfo.updateStuNum(role, stuId, updateStuNum);
-    alert("학번이 변경되었습니다.");
-  };
-  const stuNameUpdate = async () => {
-    const role = await localStorage.getItem("role");
-    await stuInfo.updateStuName(role, stuId, updateName);
-    alert("이름이 변경되었습니다.");
-  };
-  const stuRoleUpdate = async () => {
-    const role = await localStorage.getItem("role");
-    await stuInfo.updateStuRole(role, stuId, returnRoleValue(updateRole));
-    alert("권한이 변경되었습니다.");
-  };
+	const stuNumUpdate = async () => {
+		const role = await localStorage.getItem('role');
+		await stuInfo.updateStuNum(role, stuId, updateStuNum);
+		alert('학번이 변경되었습니다.');
+	};
+	const stuNameUpdate = async () => {
+		const role = await localStorage.getItem('role');
+		await stuInfo.updateStuName(role, stuId, updateName);
+		alert('이름이 변경되었습니다.');
+	};
+	const stuRoleUpdate = async () => {
+		const role = await localStorage.getItem('role');
+		await stuInfo.updateStuRole(role, stuId, returnRoleValue(updateRole));
+		alert('권한이 변경되었습니다.');
+	};
 
-  const onCancle = () => {
-    setUpdateStuNum(stuNum);
-    setUpdateName(name);
-    setUpdateRole(role);
-    closeModal();
-  };
-  const onComplete = async () => {
-    closeModal();
-    window.location.reload();
-  };
+	const onCancle = () => {
+		setUpdateStuNum(stuNum);
+		setUpdateName(memberName);
+		setUpdateRole(role);
+		closeModal();
+	};
+	const onComplete = async () => {
+		closeModal();
+		window.location.reload();
+	};
 
-  return modalState ? (
-    <>
-      <S.Positioner>
-        <S.Overlay onClick={closeModal} />
-        <S.Container>
-          <S.ChangerContainer>
-            <S.ChangerItem>
-              <S.StuNumInput
-                type="number"
-                value={updateStuNum}
-                onChange={(e) => setUpdateStuNum(e.target.value)}
-              />
-              <S.UpdateBtn onClick={stuNumUpdate}>학번 수정</S.UpdateBtn>
-            </S.ChangerItem>
-            <S.ChangerItem>
-              <S.StuNameInput
-                type="text"
-                value={updateName}
-                onChange={(e) => setUpdateName(e.target.value)}
-              />
-              <S.UpdateBtn onClick={stuNameUpdate}>이름 수정</S.UpdateBtn>
-            </S.ChangerItem>
-            <S.ChangerItem>
-              <S.StuRoleSelect
-                onChange={(e) => {
-                  setUpdateRole(e.target.value);
-                }}
-              >
-                <option value="" selected disabled hidden>
-                  선택
-                </option>
-                <option value="학생">학생</option>
-                <option value="자치위원">자치위원</option>
-                <option value="개발자">개발자</option>
-              </S.StuRoleSelect>
-              <S.UpdateBtn onClick={stuRoleUpdate}>권한 수정</S.UpdateBtn>
-            </S.ChangerItem>
-          </S.ChangerContainer>
-          <S.BtnWrapper>
-            <S.CancelBtn onClick={onCancle}>취소</S.CancelBtn>
-            <S.CompleteBtn onClick={onComplete}>완료</S.CompleteBtn>
-          </S.BtnWrapper>
-        </S.Container>
-      </S.Positioner>
-    </>
-  ) : (
-    <></>
-  );
+	return modalState ? (
+		<>
+			<S.Positioner>
+				<S.Overlay onClick={closeModal} />
+				<S.Container>
+					<S.ChangerContainer>
+						<S.ChangerItem>
+							<S.StuNumInput
+								type="number"
+								value={updateStuNum}
+								onChange={(e) => setUpdateStuNum(e.target.value)}
+							/>
+							<S.UpdateBtn onClick={stuNumUpdate}>학번 수정</S.UpdateBtn>
+						</S.ChangerItem>
+						<S.ChangerItem>
+							<S.StuNameInput
+								type="text"
+								value={updateName}
+								onChange={(e) => setUpdateName(e.target.value)}
+							/>
+							<S.UpdateBtn onClick={stuNameUpdate}>이름 수정</S.UpdateBtn>
+						</S.ChangerItem>
+						<S.ChangerItem>
+							<S.StuRoleSelect
+								onChange={(e) => {
+									setUpdateRole(e.target.value);
+								}}
+							>
+								<option value="" selected disabled hidden>
+									선택
+								</option>
+								<option value="학생">학생</option>
+								<option value="자치위원">자치위원</option>
+								<option value="개발자">개발자</option>
+							</S.StuRoleSelect>
+							<S.UpdateBtn onClick={stuRoleUpdate}>권한 수정</S.UpdateBtn>
+						</S.ChangerItem>
+					</S.ChangerContainer>
+					<S.BtnWrapper>
+						<S.CancelBtn onClick={onCancle}>취소</S.CancelBtn>
+						<S.CompleteBtn onClick={onComplete}>완료</S.CompleteBtn>
+					</S.BtnWrapper>
+				</S.Container>
+			</S.Positioner>
+		</>
+	) : (
+		<></>
+	);
 };
 
 export default InfoUpdateModal;
