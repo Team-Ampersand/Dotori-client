5701e50cd173010f86a2d596edac5f8bdb5665df
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-03-12 14:05:24 +0900

    Update Code Splitting

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,42 +1,141 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from 'Utils/Libs/PrivateRoute';
 import PublicRoute from 'Utils/Libs/PublicRoute';
-
-import {
-	HomePage,
-	SelfStudyPage,
-	SongPage,
-	NoticePage,
-	SigninPage,
-	SignupPage,
-	NoticeWritePage,
-	AuthorizationPage,
-	PwChangePage,
-	WithdrawlPage,
-	NotFoundPage,
-	PromotionPage,
-	PenaltyPage,
-	MassagePage,
-} from './Pages';
+const HomePage = lazy(() => import('./Pages/HomePage/HomePage'));
+const PromotionPage = lazy(() => import('./Pages/PromotionPage/PromotionPage'));
+const SelfStudyPage = lazy(() => import('./Pages/SelfStudyPage/SelfStudyPage'));
+const NoticePage = lazy(() => import('./Pages/NoticePage/NoticePage'));
+const NoticeWritePage = lazy(
+	() => import('./Pages/NoticeWritePage/NoticeWritePage')
+);
+const SongPage = lazy(() => import('./Pages/SongPage/SongPage'));
+const SigninPage = lazy(() => import('./Pages/SigninPage/SigninPage'));
+const SignupPage = lazy(() => import('./Pages/SignupPage/SignupPage'));
+const PwChangePage = lazy(() => import('./Pages/PwChangePage/PwChangePage'));
+const AuthorizationPage = lazy(
+	() => import('./Pages/AuthorizationPage/AuthorizationPage')
+);
+const WithdrawlPage = lazy(() => import('./Pages/WithdrawlPage/WithdrawlPage'));
+const PenaltyPage = lazy(() => import('./Pages/PenaltyPage/PenaltyPage'));
+const MassagePage = lazy(() => import('./Pages/MassagePage/MassagePage'));
+const NotFoundPage = lazy(() => import('./Pages/NotFoundPage/NotFoundPage'));
 
 const Router: React.FC = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<PublicRoute restricted={false}><PromotionPage/></PublicRoute>} />
-			<Route path="/home" element={<PrivateRoute><HomePage/></PrivateRoute>}/>
-			<Route path="/selfstudy" element={<PrivateRoute><SelfStudyPage/></PrivateRoute>}/>
-			<Route path="/notice" element={<PrivateRoute><NoticePage/></PrivateRoute>}/>						
-			<Route path="/notice/write" element={<PrivateRoute><NoticeWritePage/></PrivateRoute>}/>
-			<Route path="/song" element={<PrivateRoute><SongPage/></PrivateRoute>}/>
-			<Route path="/signin" element={<PublicRoute restricted={false}><SigninPage/></PublicRoute>}/>
-			<Route path="/signup" element={<PublicRoute restricted={false}><SignupPage /></PublicRoute>}/>
-			<Route path="/password" element={<PublicRoute restricted={false}><PwChangePage/></PublicRoute>}/>
-			<Route path="/authorization" element={<PrivateRoute><AuthorizationPage/></PrivateRoute>}/>
-			<Route path="/Withdrawl" element={<PrivateRoute><WithdrawlPage/></PrivateRoute>}/>
-			<Route path="/change/password" element={<PrivateRoute><PwChangePage/></PrivateRoute>}/>																								
-			<Route path="/penalty" element={<PrivateRoute><PenaltyPage/></PrivateRoute>}/>
-			<Route path="/massage" element={<PrivateRoute><MassagePage/></PrivateRoute>}/>
+			<Route
+				path="/"
+				element={
+					<PublicRoute restricted={false}>
+						<PromotionPage />
+					</PublicRoute>
+				}
+			/>
+			<Route
+				path="/home"
+				element={
+					<PrivateRoute>
+						<HomePage />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path="/selfstudy"
+				element={
+					<PrivateRoute>
+						<SelfStudyPage />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path="/notice"
+				element={
+					<PrivateRoute>
+						<NoticePage />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path="/notice/write"
+				element={
+					<PrivateRoute>
+						<NoticeWritePage />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path="/song"
+				element={
+					<PrivateRoute>
+						<SongPage />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path="/signin"
+				element={
+					<PublicRoute restricted={false}>
+						<SigninPage />
+					</PublicRoute>
+				}
+			/>
+			<Route
+				path="/signup"
+				element={
+					<PublicRoute restricted={false}>
+						<SignupPage />
+					</PublicRoute>
+				}
+			/>
+			<Route
+				path="/password"
+				element={
+					<PublicRoute restricted={false}>
+						<PwChangePage />
+					</PublicRoute>
+				}
+			/>
+			<Route
+				path="/authorization"
+				element={
+					<PrivateRoute>
+						<AuthorizationPage />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path="/Withdrawl"
+				element={
+					<PrivateRoute>
+						<WithdrawlPage />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path="/change/password"
+				element={
+					<PrivateRoute>
+						<PwChangePage />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path="/penalty"
+				element={
+					<PrivateRoute>
+						<PenaltyPage />
+					</PrivateRoute>
+				}
+			/>
+			<Route
+				path="/massage"
+				element={
+					<PrivateRoute>
+						<MassagePage />
+					</PrivateRoute>
+				}
+			/>
 			<Route element={<NotFoundPage />} />
 		</Routes>
 	);
