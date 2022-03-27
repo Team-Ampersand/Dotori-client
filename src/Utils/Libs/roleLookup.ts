8cbea937c1a6d062e73f3b7b1806810e133cdf0e
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-03-27 16:20:59 +0900

    Remove 이제는 더 이상 사용되지 않는 roleLookup 함수 삭제

--- src/Utils/Libs/roleLookup.ts
+++
@@ -1,8 +0,0 @@
-export const rolelookup = async (role: string) => {
-	let Role = '';
-	if (role === 'ROLE_MEMBER') Role = 'member';
-	if (role === 'ROLE_ADMIN') Role = 'admin';
-	if (role === 'ROLE_DEVELOPER') Role = 'developer';
-	if (role === 'ROLE_COUNCILLOR') Role = 'councillor';
-	return Role;
-};
