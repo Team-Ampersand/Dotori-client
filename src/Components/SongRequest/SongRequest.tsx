b9a81569ddc8b2c5de421ad7333784e201d54173
JeongTaehwan <wjdxoghks11222@naver.com>
2022-04-09 22:27:30 +0900

    Update : SongRequest 리디자인

--- src/Components/SongRequest/SongRequest.tsx
+++ src/Components/SongRequest/SongRequest.tsx
@@ -105,9 +105,23 @@ const SongRequest: React.FC = () => {
 
 	return (
 		<S.Postioner>
-			<label>기상음악 신청</label>
+			<label>※ 음악 신청 시 공지사항</label>
+			<S.TextContainer>
+				<p>
+					1. 기상 음악 순서는 신청 순서와는 별개로 사감선생님께서 정하십니다.
+				</p>
+				<p>
+					2. 영어, 한국어 이외의 언어가 포함되거나 과도한 욕설이 담긴 음악은
+					삭제될 수 있습니다.
+				</p>
+				<p>3. 기상 음악과 관련 없는 영상을 올렸을 시 삭제될 수 있습니다.</p>
+				<p>4. 노래 신청은 1일 1곡만 가능합니다.</p>
+				<p>5. 노래 신청은 취소 후 재신청이 가능합니다.</p>
+				<hr />
+			</S.TextContainer>
+
 			<S.InputWrapper>
-				<p>URL 주소</p>
+				<p>기상음악 신청</p>
 				{returnBtn(url, setUrl, songInput, role)}
 			</S.InputWrapper>
 		</S.Postioner>
