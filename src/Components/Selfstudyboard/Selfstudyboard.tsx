0ee50fe128d285b62e53db86e1e6d26258db9b57
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-04-11 15:10:44 +0900

    Update 자습신청 스타일 변경

--- src/Components/Selfstudyboard/Selfstudyboard.tsx
+++ src/Components/Selfstudyboard/Selfstudyboard.tsx
@@ -6,6 +6,7 @@ import { studyInfo, selfStudy, cancelStudy } from 'Api/selfStudy';
 import { useSetRecoilState } from 'recoil';
 import { HasToken } from 'Atoms';
 import { useRole } from 'Hooks/useRole';
+import { Arrowdown } from 'Assets/Svg';
 
 const Room = {
 	currentRoom: 13,
@@ -63,122 +64,11 @@ const cancleStudy = async (setInfo, count, role) => {
 
 const returnRoomStatusNumber = (compareMax: number, compareMin: number) => {
 	if (compareMax / 2 > compareMin) {
-		return '#28C842';
+		return '#B5EAB7';
 	} else if (compareMax * (7 / 8) > compareMin) {
-		return '#F8C600';
+		return '#FFE3A1';
 	} else {
-		return '#E11A1A';
-	}
-};
-
-const returnButton = (
-	status: string,
-	setInfo: React.Dispatch<
-		React.SetStateAction<{ count: string; selfStudy_status: string }>
-	>,
-	count: number,
-	role: string
-) => {
-	let today: string = ManufactureDate('W');
-	let can = ['월', '화', '수', '목'];
-	let cant = ['금', '토', '일'];
-	let hours = new Date().getHours();
-	if (role === 'admin') {
-		return <p>사감 선생님은 자습신청을 하지 않으셔도 됩니다.</p>;
-	} else if (status === 'IMPOSSIBLE') {
-		return (
-			<S.StudyButton
-				Clicked={status}
-				count={count}
-				onClick={() => {
-					alert('자습신청이 금지되었어요');
-				}}
-			>
-				자습불가
-			</S.StudyButton>
-		);
-	} else if (
-		status === 'CAN' &&
-		count < 50 &&
-		can.indexOf(today) !== -1 &&
-		hours >= 20 &&
-		hours < 21
-	) {
-		return (
-			<S.StudyButton
-				onClick={() => {
-					applyStudy(setInfo, count, role);
-				}}
-				Clicked={status}
-				count={count}
-			>
-				자습신청
-			</S.StudyButton>
-		);
-	} else if (status === 'APPLIED') {
-		return (
-			<S.StudyButton
-				onClick={() => {
-					if (
-						window.confirm(
-							'자습을 취소 하시겠어요? 오늘 하루동안 다시 신청이 불가능 해요'
-						)
-					) {
-						cancleStudy(setInfo, count, role);
-					} else alert('자습이 취소되지 않았어요');
-				}}
-				Clicked={status}
-				count={count}
-			>
-				자습취소
-			</S.StudyButton>
-		);
-	} else if (cant.indexOf(today) !== -1 || hours < 20 || hours >= 21) {
-		return (
-			<S.StudyButton
-				Clicked={status}
-				onClick={() => {
-					alert('자습을 신청하실 수 있는 시간이 아니에요');
-				}}
-				count={count}
-			>
-				자습불가
-			</S.StudyButton>
-		);
-	} else if (count === 50) {
-		return (
-			<S.StudyButton
-				Clicked={status}
-				onClick={() => {
-					alert('50명이 넘어 신청할 수 없어요');
-				}}
-				count={count}
-			>
-				자습불가
-			</S.StudyButton>
-		);
-	} else if (status === 'CANT') {
-		return (
-			<S.StudyButton
-				Clicked={status}
-				onClick={() => {
-					alert('자습 신청이 불가능 해요');
-				}}
-				count={count}
-			>
-				자습불가
-			</S.StudyButton>
-		);
-	} else {
-		<S.StudyButton
-			Clicked={status}
-			onClick={() => {
-				alert('자습을 신청하실 수 있는 시간이 아니에요');
-			}}
-			count={count}
-		>
-			자습불가
-		</S.StudyButton>;
+		return '#F3A7A7';
 	}
 };
 
@@ -187,28 +77,127 @@ const Selfstudyboard: React.FC = () => {
 	const setLogged = useSetRecoilState(HasToken);
 	const navigate = useNavigate();
 	const role = useRole();
+	let today: string = ManufactureDate('W');
+	let can = ['월', '화', '수', '목'];
+	let cant = ['금', '토', '일'];
+	let hours = new Date().getHours();
+
+	const returnButton = () => {
+		if (role === 'admin') {
+			return <p>사감 선생님은 자습신청을 하지 않으셔도 됩니다.</p>;
+		} else if (info.selfStudy_status === 'IMPOSSIBLE') {
+			return (
+				<S.StudyButton
+					Clicked={info.selfStudy_status}
+					count={parseInt(info.count)}
+					onClick={() => {
+						alert('자습신청이 금지되었어요');
+					}}
+				>
+					자습불가
+				</S.StudyButton>
+			);
+		} else if (
+			info.selfStudy_status === 'CAN' &&
+			parseInt(info.count) < 50 &&
+			can.indexOf(today) !== -1 &&
+			hours >= 20 &&
+			hours < 21
+		) {
+			return (
+				<S.StudyButton
+					onClick={() => {
+						applyStudy(setInfo, parseInt(info.count), role);
+					}}
+					Clicked={info.selfStudy_status}
+					count={parseInt(info.count)}
+				>
+					신청
+				</S.StudyButton>
+			);
+		} else if (info.selfStudy_status === 'APPLIED') {
+			return (
+				<S.StudyButton
+					onClick={() => {
+						if (
+							window.confirm(
+								'자습을 취소 하시겠어요? 오늘 하루동안 다시 신청이 불가능 해요'
+							)
+						) {
+							cancleStudy(setInfo, parseInt(info.count), role);
+						} else alert('자습이 취소되지 않았어요');
+					}}
+					Clicked={info.selfStudy_status}
+					count={parseInt(info.count)}
+				>
+					신청취소
+				</S.StudyButton>
+			);
+		} else if (cant.indexOf(today) !== -1 || hours < 20 || hours >= 21) {
+			return (
+				<S.StudyButton
+					Clicked={info.selfStudy_status}
+					onClick={() => {
+						alert('자습 신청이 가능한 시간이 아니에요');
+					}}
+					count={parseInt(info.count)}
+				>
+					신청
+				</S.StudyButton>
+			);
+		} else if (parseInt(info.count) === 50) {
+			return (
+				<S.StudyButton
+					Clicked={info.selfStudy_status}
+					onClick={() => {
+						alert('50명이 넘어 신청할 수 없어요');
+					}}
+					count={parseInt(info.count)}
+				>
+					신청
+				</S.StudyButton>
+			);
+		} else if (info.selfStudy_status === 'CANT') {
+			return (
+				<S.StudyButton
+					Clicked={info.selfStudy_status}
+					onClick={() => {
+						alert('자습 신청이 불가능 해요');
+					}}
+					count={parseInt(info.count)}
+				>
+					신청
+				</S.StudyButton>
+			);
+		} else {
+			<S.StudyButton
+				Clicked={info.selfStudy_status}
+				onClick={() => {
+					alert('자습을 신청하실 수 있는 시간이 아니에요');
+				}}
+				count={parseInt(info.count)}
+			>
+				신청
+			</S.StudyButton>;
+		}
+	};
 
 	useEffect(() => {
 		StudyInfo(setLogged, navigate, role).then((res) => {
 			setInfo(res?.data.data);
 		});
 	}, []);
+
 	return (
 		<S.Positioner Clicked={info.selfStudy_status} count={parseInt(info.count)}>
 			<S.StudyHeader>
-				<h2>자습신청</h2>
-				<div>
-					<Link to="/selfstudy">
-						<span>신청 현황</span>
-					</Link>
-				</div>
+				<h2>자습 신청</h2>
+				<Link to="/selfstudy">
+					<span>신청 현황</span>
+					<Arrowdown />
+				</Link>
 			</S.StudyHeader>
 			<S.StudyContent>
-				<strong>
-					{ManufactureDate('Y')}년 {ManufactureDate('M')}월{' '}
-					{ManufactureDate('D')}일 {ManufactureDate('W')}요일
-				</strong>
-				<p>20 : 00 ~ 21 : 00</p>
 				<span>
 					{parseInt(info.count)}/{Room.roomMax}
 				</span>
@@ -221,12 +210,12 @@ const Selfstudyboard: React.FC = () => {
 						count={parseInt(info.count)}
 					/>
 				</S.PointProgress>
-				{returnButton(
-					info.selfStudy_status,
-					setInfo,
-					parseInt(info.count),
-					role
-				)}
+				{returnButton()}
+				<p>
+					{cant.indexOf(today) !== -1 || hours < 20 || hours >= 21
+						? '20:00 ~ 21:00에 자습 신청이 가능해요'
+						: '자습 신청 시간이에요'}
+				</p>
 			</S.StudyContent>
 		</S.Positioner>
 	);
