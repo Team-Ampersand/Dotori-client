19ddb0a878e3763733b0c000bd841f23b02134f9
JeongTaehwan <wjdxoghks11222@naver.com>
2022-04-16 15:31:51 +0900

    Update : CheckMySong 기능 수정

--- src/Components/CheckMySong/CheckMySong.tsx
+++ src/Components/CheckMySong/CheckMySong.tsx
@@ -1,7 +1,7 @@
 import { setList } from 'Atoms';
 import SongItem from 'Components/SongItem/SongItem';
 import { useDecode } from 'Hooks/useDecode';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import * as S from './Style';
 
@@ -20,19 +20,27 @@ interface CheckMySongProps {
 const CheckMySong: React.FC<CheckMySongProps> = ({ songlists }) => {
 	const user = useDecode();
 	const [songlist, setSongList] = useRecoilState(setList);
+	const [mySong, setMySong] = useState<boolean>(false);
+
+	useEffect(() => {
+		setMySong(
+			songlist && songlist.filter((data) => data.email === user.sub).length > 0
+		);
+	}, [mySong, songlist]);
 	return (
 		<S.Positioner>
 			<label>내가 오늘 신청한 음악이에요 😉</label>
 			<S.SongContainer>
-				{songlist
-					.filter((data) => data.email === user.sub)
-					.map((item, idx) => {
-						return item.email.includes(user.sub) ? (
-							<SongItem songObj={item} key={`${idx}`} />
-						) : (
-							<p>오늘 음악 신청을 안 하셨네요 ! </p>
-						);
-					})}
+				{mySong ? (
+					songlist &&
+					songlist
+						.filter((data) => data.email === user.sub)
+						.map((item, idx) => {
+							return <SongItem songObj={item} key={`${idx}`} />;
+						})
+				) : (
+					<p>오늘은 음악 신청을 안 하셨네요!</p>
+				)}
 			</S.SongContainer>
 		</S.Positioner>
 	);
