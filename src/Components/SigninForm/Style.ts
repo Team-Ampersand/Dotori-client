d17a0df7dd3bbf5b42bff117e183b578592be00e
HAWNBINYOO <dngh0825@naver.com>
2022-04-26 23:35:41 +0900

    signin 반응형

--- src/Components/SigninForm/Style.ts
+++ src/Components/SigninForm/Style.ts
@@ -19,6 +19,22 @@ export const LogoWrapper = styled.div`
 	align-items: center;
 	justify-content: center;
 	margin-bottom: 2rem;
+
+	svg {
+		width: 100%;
+		@media (max-width: 1920px) {
+			width: 100%;
+		}
+
+		@media (max-width: 1400px) {
+			width: 80%;
+			margin-bottom: -2rem;
+		}
+		@media (max-width: 700px) {
+			width: 50%;
+			margin-bottom: -5rem;
+		}
+	}
 `;
 
 export const Line = styled.div`
@@ -31,11 +47,31 @@ export const FindWrapper = styled.div`
 	margin-top: 1vh;
 	span {
 		color: #969696;
+		@media (max-width: 1920px) {
+			font-size: 16px;
+		}
+
+		@media (max-width: 1400px) {
+			font-size: 13px;
+		}
+		@media (max-width: 700px) {
+			font-size: 8px;
+		}
 	}
 	a {
 		margin-left: 5px;
 		text-decoration: none;
 		color: #9ecff2;
+		@media (max-width: 1920px) {
+			font-size: 16px;
+		}
+
+		@media (max-width: 1400px) {
+			font-size: 13px;
+		}
+		@media (max-width: 700px) {
+			font-size: 8px;
+		}
 	}
 `;
 
@@ -43,11 +79,31 @@ export const SignupWrapper = styled.div`
 	margin-top: 1rem;
 	span {
 		color: #969696;
+		@media (max-width: 1920px) {
+			font-size: 16px;
+		}
+
+		@media (max-width: 1400px) {
+			font-size: 13px;
+		}
+		@media (max-width: 700px) {
+			font-size: 8px;
+		}
 	}
 	a {
 		margin-left: 5px;
 		text-decoration: none;
 		color: #9ecff2;
+		@media (max-width: 1920px) {
+			font-size: 16px;
+		}
+
+		@media (max-width: 1400px) {
+			font-size: 13px;
+		}
+		@media (max-width: 700px) {
+			font-size: 8px;
+		}
 	}
 `;
 
@@ -55,19 +111,36 @@ export const ButtonStyle = styled.button`
 	margin-top: 3rem;
 	width: 100%;
 	height: 5.5vh;
-	border-radius: 10px;
+	border-radius: 5px;
 	outline: none;
 	border: none;
 	background-color: #9ecff2;
 	color: #fff;
 	font-size: 18px;
 	box-shadow: 0px 0px 13px rgba(0, 0, 0, 0.12);
+
+	@media (max-width: 1920px) {
+		font-size: 22px;
+		margin-top: 3rem;
+		height: 5.5vh;
+	}
+
+	@media (max-width: 1400px) {
+		font-size: 18px;
+		margin-top: 2rem;
+		height: 4.5vh;
+	}
+	@media (max-width: 700px) {
+		font-size: 15px;
+		margin-top: 1rem;
+		height: 3vh;
+	}
 `;
 
 export const InputStyle = styled.input<StyleProps>`
 	width: 100%;
 	height: 5vh;
-	border-radius: 10px;
+	border-radius: 5px;
 	outline: none;
 	border: 2px solid #9ecff2;
 	padding-left: 20px;
@@ -75,6 +148,23 @@ export const InputStyle = styled.input<StyleProps>`
 	font-size: 16px;
 	display: ${(props) => (props.displayed === true ? 'none' : 'flex')};
 	box-sizing: border-box;
+
+	@media (max-width: 1920px) {
+		padding-left: 20px;
+		font-size: 16px;
+		height: 5vh;
+	}
+
+	@media (max-width: 1400px) {
+		padding-left: 17px;
+		font-size: 14px;
+		height: 4.5vh;
+	}
+	@media (max-width: 700px) {
+		padding-left: 12px;
+		font-size: 10px;
+		height: 3vh;
+	}
 `;
 
 export const InputWrapper = styled.div`
@@ -85,6 +175,27 @@ export const InputWrapper = styled.div`
 		position: absolute;
 		top: 25px;
 		left: 80px;
+		font-size: 16px;
+
+		@media (max-width: 1920px) {
+			font-size: 16px;
+			position: absolute;
+			top: 25px;
+			left: 80px;
+		}
+
+		@media (max-width: 1400px) {
+			font-size: 14px;
+			position: absolute;
+			top: 20px;
+			left: 70px;
+		}
+		@media (max-width: 700px) {
+			font-size: 10px;
+			position: absolute;
+			top: 13px;
+			left: 50px;
+		}
 	}
 	@media screen and (min-height: 1073px) {
 		label {
