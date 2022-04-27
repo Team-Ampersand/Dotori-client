1fd69e995d94855dcbb6b0de52abcb292680bb8f
JeongTaehwan <wjdxoghks11222@naver.com>
2022-04-27 09:12:40 +0900

    Update : 디자인 수정

--- src/Pages/SongPage/Style.ts
+++ src/Pages/SongPage/Style.ts
@@ -8,7 +8,9 @@ export const Positioner = styled.div`
 
 export const Container = styled.div`
 	width: 100%;
+	height: 100%;
 	display: flex;
 	flex-direction: column;
+	row-gap: 10%;
 	margin-left: 3%;
 `;
