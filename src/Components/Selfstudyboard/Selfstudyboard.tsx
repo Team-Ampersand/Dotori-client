24c7af127996c45c19ca283892e35d9293169e35
hawnbin <dngh0825@naver.com>
2022-10-21 10:33:16 +0900

    fix 신청불가한 상황일떄 text 변경,여자 안마의자신청 ui 변경

--- src/Components/Selfstudyboard/Selfstudyboard.tsx
+++ src/Components/Selfstudyboard/Selfstudyboard.tsx
@@ -85,7 +85,7 @@ const Selfstudyboard: React.FC = () => {
 						toast.info('자습 신청이 금지되었어요');
 					}}
 				>
-					자습불가
+					신청불가
 				</S.StudyButton>
 			);
 		} else if (
@@ -144,7 +144,7 @@ const Selfstudyboard: React.FC = () => {
 					}}
 					count={parseInt(info.count)}
 				>
-					신청
+					신청불가
 				</S.StudyButton>
 			);
 		} else if (parseInt(info.count) === 50) {
@@ -156,7 +156,7 @@ const Selfstudyboard: React.FC = () => {
 					}}
 					count={parseInt(info.count)}
 				>
-					신청
+					신청불가
 				</S.StudyButton>
 			);
 		} else if (info.selfStudy_status === 'CANT') {
@@ -168,7 +168,7 @@ const Selfstudyboard: React.FC = () => {
 					}}
 					count={parseInt(info.count)}
 				>
-					신청
+					신청불가
 				</S.StudyButton>
 			);
 		} else {
@@ -180,7 +180,7 @@ const Selfstudyboard: React.FC = () => {
 					}}
 					count={parseInt(info.count)}
 				>
-					신청
+					신청불가
 				</S.StudyButton>
 			);
 		}
